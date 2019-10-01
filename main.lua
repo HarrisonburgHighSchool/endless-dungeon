9f799832403bb753a09c2a202587c1a8c53a11ec
xander <ximart79@harrisonburg.k12.va.us>
2019-10-01 09:53:48 -0400

    need help

--- main.lua
+++ main.lua
@@ -5,14 +5,19 @@ function love.load()
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
 end
 
-
 function love.update(dt)
-
+if love.keyboard.isDown('up') then  
+  y = y + 1
 end
-
+if love.keyboard.isDown('right') then  
+  x = x + 1
+end
+if love.keyboard.isDown('left') then  
+  x = x - 1
+end
 
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
-
+  
