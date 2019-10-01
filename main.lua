4f8a3ff6540db882d7863d2707e6b3cbf617734e
Harrison Dailey <hfdail79>
2019-10-01 09:54:40 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,17 +2,31 @@
 function love.load()
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  
+  playerImg = love.graphics.newImage('assets-1/misc/blood/wall_old_blood_0.png')
 end
 
 
 function love.update(dt)
+if love.keyboard.isDown('up') then
+y=y-1
+end
+if love.keyboard.isDown('down') then
+  y=y+1
+  end
+  if love.keyboard.isDown('right') then
+  x=x+1
+  end
+  if love.keyboard.isDown('left') then
+  x=x-1
+  end
 
+  
 end
 
 
 function love.draw()
-  love.graphics.print('Hello, world!', 0, 0)
-  love.graphics.draw(playerImg, x, y)
+  love.graphics.print('There is no escape', 0, 0)
+  love.graphics.draw(playerImg, x, y, 0, 4)
 end
 
