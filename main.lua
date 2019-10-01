7108ca24bcf4ef137941eb3b7174a4c2bb518871
Daniel V <davarg@harrisonburg.k12.va.us>
2019-10-01 09:54:09 -0400

    Beta V0.0.1

--- main.lua
+++ main.lua
@@ -2,17 +2,28 @@
 function love.load()
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  playerImg = love.graphics.newImage('assets-1/player/transform/dragon_form_red.png')
 end
 
 
 function love.update(dt)
-
+  if love.keyboard.isDown('d') then   -- if the 'up' key is being pressed...
+    x = x + 6
+  end
+  if love.keyboard.isDown('s') then   -- if the 'up' key is being pressed...
+    y = y + 6
+  end
+  if love.keyboard.isDown('a') then   -- if the 'up' key is being pressed...
+    x = x - 6
+  end
+  if love.keyboard.isDown('w') then   -- if the 'up' key is being pressed...
+    y = y - 6
+  end
 end
 
 
 function love.draw()
-  love.graphics.print('Hello, world!', 0, 0)
+  love.graphics.print('PooPooWeeWeeCaCa', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
 
