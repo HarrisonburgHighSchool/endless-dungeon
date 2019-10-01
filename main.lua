c892b742c1a9c3f3b702865bd86a41e48deb1a4f
Jacob <joyear62@harrisonburg.k12.va.us>
2019-10-01 09:54:12 -0400

    dungeon-game

--- main.lua
+++ main.lua
@@ -2,13 +2,25 @@
 function love.load()
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  playerImg = love.graphics.newImage('assets-1/monster/juggernaut.png')
 end
 
 
 function love.update(dt)
-
+  if love.keyboard.isDown('w') and y > 0 then   -- if the 'up' key is being pressed
+    y = y - 5
+  end
+  if love.keyboard.isDown('s') then   -- if the 'up' key is being pressed
+    y = y + 5
+  end
+  if love.keyboard.isDown('a') then   -- if the 'up' key is being pressed
+    x = x - 5
+  end
+  if love.keyboard.isDown('d') then   -- if the 'up' key is being pressed
+    x = x + 5
+  end
 end
+
 
 
 function love.draw()
