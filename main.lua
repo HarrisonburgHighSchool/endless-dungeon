843006ec7245de509c91cdc1fe2552cae41385ef
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-10-01 09:53:57 -0400

    hjyj

--- main.lua
+++ main.lua
@@ -2,14 +2,23 @@
 function love.load()
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  playerImg = love.graphics.newImage('assets-1/player/base/Link.png')
 end
-
 
 function love.update(dt)
-
+  if love.keyboard.isDown('right') then   -- if the 'up' key is being pressed...
+    x = x + 1
+  end
+  if love.keyboard.isDown('down') then   -- if the 'up' key is being pressed...
+    y = y + 1
+  end
+  if love.keyboard.isDown('left') then   -- if the 'up' key is being pressed...
+    x = x - 1
+  end
+  if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
+    y = y - 1
+  end
 end
-
 
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
