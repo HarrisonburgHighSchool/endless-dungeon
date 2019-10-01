d413e146e25b77c0d793f69a6c796443d479513d
sam <sostep99@harrisonburg.k12.va.us>
2019-10-01 09:53:56 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,12 +2,23 @@
 function love.load()
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  playerImg = love.graphics.newImage('assets-1/player/base/formicid.png')
 end
 
 
 function love.update(dt)
-
+  if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
+    y = y - 5
+  end
+  if love.keyboard.isDown('down') then   -- if the 'up' key is being pressed...
+    y = y + 5
+  end
+  if love.keyboard.isDown('left') then   -- if the 'up' key is being pressed...
+    x = x - 5
+  end
+  if love.keyboard.isDown('right') then   -- if the 'up' key is being pressed...
+    x = x + 5
+  end
 end
 
 
@@ -15,4 +26,5 @@ function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
+
 
