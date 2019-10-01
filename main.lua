e9b07b1a7015375cce447ece74acf31ce83a6976
Travis <dtrode59@harrisonburg.k12.va.us>
2019-10-01 09:54:01 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,9 +7,19 @@ end
 
 
 function love.update(dt)
-
+  if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
+    x = x + 3
+  end
+  if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
+    y = y - 3
+  end
+  if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
+    y = y + 3
+  end
+  if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
+    x = x - 3
+  end
 end
-
 
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
