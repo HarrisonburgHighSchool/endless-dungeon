a004e865f35f8881d86ea072e35765a074af52be
Eric Tolliver <eitoll21@harrisonburg.k12.va.us>
2019-10-01 09:53:43 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,18 @@ end
 
 
 function love.update(dt)
-
+  if love.keyboard.isDown('up key') then   -- if the 'up' key is being pressed...
+    x = x + 1
+  end
+  if love.keyboard.isDown('right key') then   -- if the 'up' key is being pressed...
+    y = y + 1
+  end
+  if love.keyboard.isDown('down key') then   -- if the 'up' key is being pressed...
+    x = x - 1
+  end
+  if love.keyboard.isDown('left key') then   -- if the 'up' key is being pressed...
+    y = y - 1
+  end
 end
 
 
