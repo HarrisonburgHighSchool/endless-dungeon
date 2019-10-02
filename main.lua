848de36caa4ff7e69c0b74eeb4626fdb77631d9d
Logan <ldlope30@harriosnburg.k12.va.us>
2019-10-02 14:33:37 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,45 @@
 local Map = require 'core/map'
 
 function love.load()
+  a=500
+  b=400
+  playerImg1 = love.graphics.newImage('assets-1/player/base/lorc_male_2.png')
   x = 400
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+
 end
 
 function love.update(dt)
-  -- Nothing to update yet
+if love.keyboard.isDown('right') then
+x = x + 1
+end
+if love.keyboard.isDown('left') then
+x = x - 1
+end
+if love.keyboard.isDown('down') then
+y = y + 1
+end
+if love.keyboard.isDown('up') then
+y = y - 1
+end
+
+if love.keyboard.isDown('d') then
+a = a + 1
+end
+if love.keyboard.isDown('a') then
+a = a - 1
+end
+if love.keyboard.isDown('s') then
+b = b + 1
+end
+if love.keyboard.isDown('w') then
+b = b - 1
+end
 end
 
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
+  love.graphics.draw(playerImg1, a, b)
 end
