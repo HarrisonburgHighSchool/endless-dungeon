18c6f9697cc16574eb8818a22d43a09d07b97e27
Robi <rahail79@iMac714-462.local>
2019-10-02 14:32:36 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,14 +3,30 @@ local Map = require 'core/map'
 function love.load()
   x = 400
   y = 300
+  a = 400
+  b = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  playerImg1 = love.graphics.newImage('assets-1/player/base/tengu_wingless_brown_male.png')
+
 end
 
 function love.update(dt)
-  -- Nothing to update yet
+ if love.keyboard.isDown('up')then
+ y = y - 1
+ end
+ if love.keyboard.isDown('down')then
+ y = y + 1
+ end
+ if love.keyboard.isDown('right')then
+ x = x + 1
 end
-
+ if love.keyboard.isDown('left')then
+ x = x - 1
+ end
+ end
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
+  love.graphics.draw(playerImg1, a, b)
+
 end
