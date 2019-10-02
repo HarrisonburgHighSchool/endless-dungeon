14366b9a828d4b315087696b750d278996bc8b7a
Daniel V <davarg@harrisonburg.k12.va.us>
2019-10-02 09:53:17 -0400

    Beta V0.0.2

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ local Map = require 'core/map'
 function love.load()
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/transform/dragon_form_red.png')
+  playerImg = love.graphics.newImage('a/images.png')
 end
 
 
@@ -20,24 +20,21 @@ function love.update(dt)
   if love.keyboard.isDown('w') then   -- if the 'up' key is being pressed...
     y = y - 6
   end
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
+  mapTemplate = {
+    {cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt},
+  }
 
-  --cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
- -- mapTemplate = {
-    --{cobalt, cobalt, cobalt},
-   -- {cobalt, cobalt, cobalt},
-   -- {cobalt, cobalt, cobalt},
-  --}
+  map = Map:new(mapTemplate)
+end
 
-  --map = Map:new(mapTemplate)
+--function love.update(dt)
+  -- Nothing to update yet
 --end
 
-function love.update(dt)
-  -- Nothing to update yet
-end
-
 function love.draw()
-  love.graphics.print('PooPooWeeWeeCaCa', 0, 0)
+  love.graphics.draw(cobalt)
   love.graphics.draw(playerImg, x, y)
 end
-
