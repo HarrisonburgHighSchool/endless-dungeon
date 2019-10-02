959e1fbb192e63fb66bef51a1351c2f04c15564e
Nana Kofi Darko <nodark70@iMac720-462.local>
2019-10-02 14:34:03 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,14 +2,41 @@ function love.load()
   x = 400
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+
+  x = 500
+  y = 300
+playerImg1 = love.graphics.newImage('assets-1/player/base/gargoyle_female.png')
 end
+
+
 
 function love.update(dt)
   -- Nothing to update yet
+  if love.keyboard.isDown('right') then
+    x = x + 1
+  end
+
+  if love.keyboard.isDown('left') then
+    x = x - 1
+  end
+
+  if love.keyboard.isDown('down') then
+    y = y + 1
+  end
+
+  if love.keyboard.isDown('up') then
+    y = y - 1
+  end
+
+
+
+
+
 end
 
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
-end
+  love.graphics.draw(playerImg1, x, y)
 
+end
