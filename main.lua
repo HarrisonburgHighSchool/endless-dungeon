f587e461c16324b27726663e5df4177cf8f60f6c
Aidan Saunders <adsaun19@harrisonburg.k12.va.us>
2019-10-02 14:28:07 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,33 @@
+
+local Map = require 'core/map'
+
 function love.load()
-  x = 100
-  y = 200
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  x = 400
+  y = 400
+  playerImg = love.graphics.newImage('assets-1/player/base/lorc_male_6.png')
+oct = love.graphics.newImage('assets-1/dungeon/floor/sand_1.png')
+  map = Map:new(15, 15)
 end
 
 function love.update(dt)
-  -- Nothing to update yet
+  if love.keyboard.isDown('right')then
+    x = x + 3
+  end
+  if love.keyboard.isDown('left')then
+    x = x - 3
+  end
+  if love.keyboard.isDown('up')then
+    y = y - 3
+  end
+  if love.keyboard.isDown('down')then
+    y = y + 3
+  end
 end
 
 function love.draw()
+  map:draw()
   love.graphics.print('Hello, world!', 0, 0)
+  love.graphics.draw(oct, 200, 200)
   love.graphics.draw(playerImg, x, y)
+
 end
