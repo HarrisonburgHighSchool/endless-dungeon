0fb3db4fe309ad80202a483e8281aea28744fd82
Eric Tolliver <eitoll21@harrisonburg.k12.va.us>
2019-10-02 09:53:03 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
-
+local Map = require 'core/map'
 function love.load()
+  map = Map:new(15, 15) -- Create a 5 x 5 map object named "map"
   x = 400
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
@@ -7,22 +8,24 @@ end
 
 
 function love.update(dt)
-  if love.keyboard.isDown('up key') then   -- if the 'up' key is being pressed...
+  if love.keyboard.isDown('right') then   -- if the 'up' key is being pressed...
     x = x + 1
   end
-  if love.keyboard.isDown('right key') then   -- if the 'up' key is being pressed...
+  if love.keyboard.isDown('down') then   -- if the 'up' key is being pressed...
     y = y + 1
   end
-  if love.keyboard.isDown('down key') then   -- if the 'up' key is being pressed...
+  if love.keyboard.isDown('left') then   -- if the 'up' key is being pressed...
     x = x - 1
   end
-  if love.keyboard.isDown('left key') then   -- if the 'up' key is being pressed...
+  if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
     y = y - 1
   end
 end
 
+local Map = require 'core/map'
 
 function love.draw()
+  map:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
