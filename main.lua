c028839fe310de51728b198cf011d5ec4f63e936
Abou <Asdiop17@harrisonburg.k12.va.us>
2019-10-02 09:53:08 -0400

    map attempt

--- main.lua
+++ main.lua
@@ -5,6 +5,22 @@ function love.load()
   playerImg = love.graphics.newImage('assets-1/player/base/gargoyle_male.png')
 end
 
+local Map = require 'core/map'
+
+function love.load()
+  map = Map:new(5, 5) -- Create a 5 x 5 map object named "map"
+end
+
+map = love.graphics.newImage('asset.png')
+
+
+
+
+
+
+
+  map = Map:new(template)
+end
 
 function love.update(dt)
   if love.keyboard.isDown('up') and y > 0 then   -- if the 'up' key is being pressed...
