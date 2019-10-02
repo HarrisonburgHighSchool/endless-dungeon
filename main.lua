24c4ac6d700e83e9c54e0264875efbc173b667ee
xander <ximart79@harrisonburg.k12.va.us>
2019-10-02 09:53:22 -0400

    finished the map

--- main.lua
+++ main.lua
@@ -1,22 +1,29 @@
+local Map = require 'core/map'
 
 function love.load()
+  map = Map:new(5, 5)
   x = 400
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
 end
 
 function love.update(dt)
-if love.keyboard.isDown('up') then  
-  y = y + 1
+  if love.keyboard.isDown('up') then  
+    y = y - 1
+  end
+  if love.keyboard.isDown('right') then  
+    x = x + 1
+  end
+  if love.keyboard.isDown('left') then  
+    x = x - 1
+  end
+if love.keyboard.isDown('down') then  
+    y = y + 1
 end
-if love.keyboard.isDown('right') then  
-  x = x + 1
-end
-if love.keyboard.isDown('left') then  
-  x = x - 1
 end
 
 function love.draw()
+  map:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
