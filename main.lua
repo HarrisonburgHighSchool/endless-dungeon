e81cee0170cac033fd7830d352cafce5d5ff4423
david <dtkoml19@harrisonburg.k12.va.us>
2019-10-02 14:33:45 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,44 @@
 local Map = require 'core/map'
 
 function love.load()
+
   x = 400
   y = 300
+  a = 400
+  b = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+end
+  playerImg2 = love.graphics.newImage('blue_devil.png')
 end
 
 function love.update(dt)
   if love.keyboard.isDown('right') then
-    x = x + 1
+    x=x+1
+  end
+  if love.keyboard.isDown('left') then
+    x=x-1
+  end
+  if love.keyboard.isDown('up') then
+    y=y-1
+  end
+  if love.keyboard.isDown('down') then
+    y=y+1
+  end
+  if love.keyboard.isDown('right') then
+    a=a+1
+  end
+  if love.keyboard.isDown('left') then
+    a=a-1
+  end
+  if love.keyboard.isDown('up') then
+    b=b-1
+  end
+  if love.keyboard.isDown('down') then
+    b=b+1
   end
 end
+
+
 
 
 function love.draw()
