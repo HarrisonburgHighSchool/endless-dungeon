77d6dc13525b2058aa326ca169b97f698c131d5f
Jon Stapleton <jstapleton@harrisonburg.k12.va.us>
2019-10-02 13:28:41 -0400

    ipdatgdea

--- main.lua
+++ main.lua
@@ -4,15 +4,6 @@ function love.load()
   x = 400
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
-
-  cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
-  mapTemplate = {
-    {cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt},
-  }
-
-  map = Map:new(mapTemplate)
 end
 
 function love.update(dt)
@@ -21,7 +12,5 @@ end
 
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
-  map:draw()
   love.graphics.draw(playerImg, x, y)
 end
-
