ebc3a5a3e4b9915c0e49edb68b5a5aacf6045c03
Harrison Dailey <hfdail79>
2019-10-02 09:45:41 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,32 +1,38 @@
+local Map = require 'core/map'
 
 function love.load()
-  x = 400
-  y = 300
   
-  playerImg = love.graphics.newImage('assets-1/misc/blood/wall_old_blood_0.png')
+  wall = love.graphics.newImage('assets-1/dungeon/wall/crystal_wall_yellow.png')
+  floor = love.graphics.newImage('assets-1/dungeon/floor/white_marble_0.png')
+  template = {
+    {floor,wall,floor,floor},
+    {wall,wall,wall,wall},
+    {floor,wall,floor,floor},}
+  map = Map:new(5, 5)
 end
 
 
 function love.update(dt)
-if love.keyboard.isDown('up') then
-y=y-1
-end
-if love.keyboard.isDown('down') then
-  y=y+1
-  end
-  if love.keyboard.isDown('right') then
-  x=x+1
-  end
-  if love.keyboard.isDown('left') then
-  x=x-1
-  end
+  --if love.keyboard.isDown('up') then
+  --y=y-1
+  --end
+  --if love.keyboard.isDown('down') then
+  --y=y+1
+  --end
+  --if love.keyboard.isDown('right') then
+  --x=x+1
+  --end
+  --if love.keyboard.isDown('left') then
+  --x=x-1
+  --end
 
   
 end
 
 
 function love.draw()
-  love.graphics.print('There is no escape', 0, 0)
-  love.graphics.draw(playerImg, x, y, 0, 4)
+  
+  map:draw()
+
 end
 
