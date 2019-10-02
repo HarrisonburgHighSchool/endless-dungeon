6494b898265ee8b23b5b39ce4fb96ef7593edf57
Philip <pvlevc59@harrisonburg.k12.va.us>
2019-10-02 14:33:24 -0400

    asdsad

--- main.lua
+++ main.lua
@@ -3,14 +3,36 @@ local Map = require 'core/map'
 function love.load()
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  playerImg = love.graphics.newImage('assets-1/monster/ironheart_preserver.png')
+  a = 500
+  b = 400
+  Img2 = love.graphics.newImage('assets-1/monster/statues/chilling_statue.png')
+  c = 300
+  d = 400
+  Img3 = love.graphics.newImage('assets-1/monster/statues/chilling_statue.png')
 end
 
+
+
+
 function love.update(dt)
-  -- Nothing to update yet
+  if love.keyboard.isDown('right') then
+    x = x + 1
+  end
+    if love.keyboard.isDown('down') then
+      y = y + 1
+  end
+    if love.keyboard.isDown('up') then
+      y = y - 1
+    end
+  if love.keyboard.isDown('left') then
+    x = x - 1
+  end
 end
 
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
+  love.graphics.draw(Img2, a, b)
+  love.graphics.draw(Img3, c, d)
 end
