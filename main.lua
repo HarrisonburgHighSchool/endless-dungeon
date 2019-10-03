df7b8b88413486ba3eaa14b53474c5b8dcdea275
Precious <kpcast48@harrisonburg.k12.va.us>
2019-10-03 09:53:07 -0400

    Statue
    placing objects

--- main.lua
+++ main.lua
@@ -3,29 +3,31 @@ local Map = require 'core/map'
 function love.load()
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-2/player/base/elf_f.png')
-  
-  ,limestone,limestone = love.graphics.newImage('assets-1/dungeon/floor/limestone_1.png')
+  playerImg = love.graphics.newImage('assets-2/dc-mon/glowing_shapeshifter.png')
+  statue = love.graphics.newImage('assets-1/dungeon/statues/statue_angel.png')
+  limestone = love.graphics.newImage('assets-1/dungeon/floor/limestone_1.png')
   
   mapTemplate = {
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
-    {,limestone,limestone, ,limestone,limestone, ,limestone,limestone, ,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
+    {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
     
   }
 
   map = Map:new(mapTemplate)
 end
+
+
 
 function love.update(dt)
   if love.keyboard.isDown('down') then   -- if the 'up' key is being pressed...
@@ -45,6 +47,11 @@ end
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   map:draw()
+  love.graphics.draw(statue, 400, 270)
   love.graphics.draw(playerImg, x, y)
+ 
+  
+  
+
 end
 
