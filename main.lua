1b1233b089a70a5ca8032c26fcd698e48114c4af
Corey <cdrupe35@harrisonburg.k12.va.us>
2019-10-03 09:52:51 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,28 @@
 local Map = require 'core/map'
 
 function love.load()
-
+enter = 1
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/monster/raven.png')
+  playerImg = love.graphics.newImage('assets-1/monster/download.png')
 
-  floor = love.graphics.newImage('assets-1/dungeon/floor/etched_1.png')
+  floor = love.graphics.newImage('assets-1/dungeon/floor/cobble_blood_2.png')
+  floor1 = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
+  floor2 = love.graphics.newImage('assets-1/dungeon/floor/cobble_blood_12.png')
   template = { 
-               {floor, floor, floor},
-               {floor, floor, floor},
-               {floor, floor, floor},
-               {floor, floor, floor},
+               {floor1, floor1, floor1, floor1, floor, floor1, floor1, floor1, floor1},
+               {floor1, floor2, floor2, floor, floor, floor, floor2, floor2, floor1},
+               {floor1, floor2, floor, floor, floor, floor, floor, floor2, floor1},
+               {floor1, floor, floor2, floor, floor, floor, floor2, floor, floor1},
+               {floor1, floor, floor, floor, floor, floor, floor, floor, floor1},
+               {floor1, floor, floor, floor2, floor, floor2, floor, floor, floor1},
+               {floor1, floor, floor, floor, floor, floor2, floor, floor, floor1},
+               {floor1, floor, floor2, floor, floor, floor, floor2, floor, floor1},
+               {floor1, floor, floor, floor, floor2, floor, floor, floor, floor1},
+               {floor1, floor, floor, floor, floor, floor, floor, floor, floor1},
+               {floor1, floor2, floor, floor, floor, floor, floor, floor2, floor1},
+               {floor1, floor2, floor2, floor, floor, floor, floor2, floor2, floor1},
+               {floor1, floor1, floor1, floor1, floor1, floor1, floor1, floor1, floor1},
              }
     map = Map:new(template)
 end
@@ -29,16 +40,16 @@ end
   if love.keyboard.isDown('down') then 
     y = y + 3
 end
-if(x < 1)then 
+if(x < 62)then 
     x = x + 4
 end
 if(x > 730)then 
   x = x - 4
 end
-if(y < 1)then 
+if(y < 62)then 
   y = y + 4
 end
-if(y > 550)then 
+if(y > 480)then 
   y = y - 4
 end
 end
@@ -46,4 +57,5 @@ end
 function love.draw()
   map:draw()
   love.graphics.draw(playerImg, x, y)
+
 end
