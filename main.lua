94549ffd5adb7356ec6df2a2dc60172b8bb1053c
Daniel V <davarg@harrisonburg.k12.va.us>
2019-10-03 09:48:01 -0400

    Beta V0.0.3

--- main.lua
+++ main.lua
@@ -1,8 +1,27 @@
-
+local Map = require 'core/map'
 function love.load()
+  map = Map:new(20,20)
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('a/images.png')
+  playerImg = love.graphics.newImage('assets-1/player/transform/dragon_form_red.png')
+  cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
+  template = {
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+  }
+
+  map = Map:new(template)
 end
 
 
@@ -19,14 +38,6 @@ function love.update(dt)
   if love.keyboard.isDown('w') then   -- if the 'up' key is being pressed...
     y = y - 6
   end
-  cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
-  mapTemplate = {
-    {cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt},
-  }
-
-  map = Map:new(mapTemplate)
 end
 
 --function love.update(dt)
@@ -34,6 +45,6 @@ end
 --end
 
 function love.draw()
-  love.graphics.draw(cobalt)
+  map:draw()
   love.graphics.draw(playerImg, x, y)
 end
