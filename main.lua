ea8c4eec9cc48ae5aad7c73a705df08c8a0ddf01
Don <Dcdegr59@Harrisonburg.k12.va.us>
2019-10-03 09:22:52 -0400

    Manage to make the map big

--- main.lua
+++ main.lua
@@ -9,22 +9,22 @@ function love.load()
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_2.png')
 
-  map = Map:new(5, 5) -- Create a 5 x 5 map object named "map"
+  map = Map:new(14, 14) -- Create a 14 x 14 map object named "map"
 end
 
 
 function love.update(dt)
  if love.keyboard.isDown('right') then  
-x = x + 1
+x = x + 21
 end
 if love.keyboard.isDown('down') then  
-y = y + 1
+y = y + 21
 end
 if love.keyboard.isDown('left') then  
-x = x - 1
+x = x - 21
 end
 if love.keyboard.isDown('up') then 
-  y = y - 1
+  y = y - 21
 end
 
 
