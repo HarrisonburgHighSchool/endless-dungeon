5fd6d5a08b8a38134c48c9ed3950e7c184735e61
Brian <besont40@harrisonburg.k12.va.us>
2019-10-03 09:57:56 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -21,15 +21,15 @@ if love.keyboard.isDown('s') then   -- if the 'up' key is being pressed...
 end
 if love.keyboard.isDown('a') then   -- if the 'up' key is being pressed...
   x = x - 5
-  cam:setPosition(300,400)
-  cam = gamera.new(0, 0, 2000, 2000) -- Create a camera that can move in a rectangle from 0, 0 to 2000, 2000
+  
 end
+cam:setPosition(x,y)
 end
 
 function love.draw()
-  map:draw()
   cam:draw(function(l, t, w, h)
-  love.graphics.print('Hi how has your day!', 0, 0)
-  love.graphics.draw(playerImg, x, y)
+    map:draw()
+    love.graphics.print('Hi how has your day!', 0, 0)
+    love.graphics.draw(playerImg, x, y)
 end)
 end
