3e95743bfa49aff1a582eef3e40d4c7c712e23a1
Logan <ldlope30@harriosnburg.k12.va.us>
2019-10-03 14:33:34 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,16 @@
-
+  local Map = require 'core/map'
+  love.graphics.setDefaultFliter('nearest', 'nearest')
 function love.load()
-  a=500
-  b=400
-  playerImg1 = love.graphics.newImage('assets-1/player/base/lorc_male_2.png')
-  x = 400
-  y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
 
+floorTile = love.graphics.newImage('assets-1/dungeon/floor/grass/grass_2.png')
+altar     = love.graphics.newImage('assets-1/dungeon/floor/dirt_0.png')
+template = { --a 3 x 3 map with the altar texture in the middle
+             {floorTile, floorTile,,
+             {floorTile, altar, },
+             {floorTile, floorTile, },
+           }
+map = Map:new(template)
 end
-
 function love.update(dt)
 if love.keyboard.isDown('right') then
 x = x + 1
@@ -38,6 +40,7 @@ end
 end
 
 function love.draw()
+map:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
   love.graphics.draw(playerImg1, a, b)
