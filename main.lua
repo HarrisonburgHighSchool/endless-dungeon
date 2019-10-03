15c4bd17c4bd9d3284b91bd443ed1382bf82c33c
jerry437 <50630964+jerry437@users.noreply.github.com>
2019-10-03 14:33:46 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-
+local Map = require 'core/map'
 function love.load()
   x = 400
   y = 300
@@ -6,35 +6,55 @@ function love.load()
   B = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
   img = love.graphics.newImage('assets-1/item/book/plaid.png')
+  floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_7.png')
+  altar     = love.graphics.newImage('assets-1/dungeon/floor/bog_green_2.png')
+template = {
+             {floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+             {floorTile, altar, floorTile,floorTile, altar, floorTile, floorTile, altar, floorTile,floorTile, altar, floorTile},
+             {floorTile, floorTile, floorTile,floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+             {floorTile, altar, floorTile,floorTile, altar, floorTile, floorTile, altar, floorTile,floorTile, altar, floorTile},
+             {floorTile, floorTile, floorTile,floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+             {floorTile, altar, floorTile,floorTile, altar, floorTile,floorTile, altar, floorTile,floorTile, altar, floorTile},
+             {floorTile, floorTile, floorTile,floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile},
+             {floorTile, altar, floorTile,floorTile, altar, floorTile,floorTile, altar, floorTile,floorTile, altar, floorTile},
+             {floorTile, floorTile, floorTile,floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+             }
+  map = Map:new(template)
 end
 
+
 function love.update(dt)
+
   if love.keyboard.isDown('up')then
-    y=y-1
+    y=y-3
   end
   if love.keyboard.isDown('down')then
-    y=y+1
+    y=y+3
   end
   if love.keyboard.isDown('left')then
-    x=x-1
+    x=x-3
   end
   if love.keyboard.isDown('right')then
-    x=x+1
+    x=x+3
   end
   if love.keyboard.isDown('w')then
-    B=B-1
+    B=B-3
   end
   if love.keyboard.isDown('s')then
-    B=B+1
+    B=B+3
   end
   if love.keyboard.isDown('a')then
-    A=A-1
+    A=A-3
   end
   if love.keyboard.isDown('d')then
-    A=A+1
+    A=A+3
   end
 end
 function love.draw()
+  map:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
   love.graphics.draw(img, A, B, 0, 2)
