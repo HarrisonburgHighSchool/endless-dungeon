bcc282fe37a2330756f5a5605d83c4ddb4798fd0
Jackson Calhoun <jwcalh19@harrisonburg.k12.va.us>
2019-10-03 09:54:09 -0400

    rrr

--- main.lua
+++ main.lua
@@ -1,26 +1,32 @@
-
+local Map = require 'core/map'
 function love.load()
+ 
+ 
+  fountain = love.graphics.newImage('assets-2/dc-dngn/dngn_blue_fountain2.png')
+  fountain2 = love.graphics.newImage('assets-2/dc-dngn/dngn_blood_fountain.png')
+  dragon = love.graphics.newImage('assets-1/dungeon/statues/statue_dragon.png')
   x = 400
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
- 
-  fountain = love.graphics.newImage('assets-2/dc-dngn/dngn_blue_fountain.png')
-  dragon = love.graphics.newImage('assets-1/dungeon/statues/statue_dragon.png')
   floor = love.graphics.newImage('assets-1/dungeon/floor/crypt_domino_2a.png')
+  wall = love.graphics.newImage('assets-2/dc-dngn/wall/brick_dark3.png')
+  door = love.graphics.newImage('assets-2/dc-dngn/dngn_open_door.png')
+  doorc = love.graphics.newImage('assets-2/dc-dngn/dngn_closed_door.png')
+
   template = {
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
-    {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
+    {wall, wall, wall, wall, wall, wall, wall, wall, doorc, wall},
+    {door, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, floor, floor, floor, floor, floor, floor, floor, floor, wall},
+    {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
 
   }
 map = Map:new(template)
@@ -44,8 +50,10 @@ end
 
 
 function love.draw()
-  love.graphics.print('Hello, world!', 0, 0)
-  love.graphics.draw(playerImg, x, y)
-  love.graphics.draw(fountain, 400, 100)
+  map:draw()
+  --love.graphics.print('Hello, world!', 0, 0)
+  love.graphics.draw(fountain, 675, 100)
+  love.graphics.draw(fountain2, 675, 500)
   love.graphics.draw(dragon, 400, 200)
+  love.graphics.draw(playerImg, x, y)
 end
