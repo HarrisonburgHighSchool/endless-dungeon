39f12040c02cd255b61e0813b56938c8219ad33f
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-10-04 09:37:41 -0400

    Quality of life changes to map + added Camera

--- main.lua
+++ main.lua
@@ -1,31 +1,42 @@
+local Map = require 'core/map'
+local gamera = require 'core/gamera'
 
 function love.load()
-  x = 0
-  mirrorx = 704
+  x = 64
+  camx = 96
+  mirrorx = 768
   y = 256
+  camy = 256
   mirrory = 256
 
-  playerImg = love.graphics.newImage('assets-1/player/base/Octopode_1.png')
+  cam = gamera.new(48, 48, 816, 608)
+
+  playerImg = love.graphics.newImage('assets-1/player/base/Octopode_3.png')
   mirrorPlayerImg = love.graphics.newImage('assets-1/player/base/Octopode_2.png')
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/limestone_6.png')
+  cobaltM = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_flip.png')
   cobaltMirrorLeft = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_left.png')
+  cobaltMirrorLeftT = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_left_top.png')
   cobaltMirrorRight = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_right.png')
+  cobaltMirrorRightT = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_right_top.png')
   hole = love.graphics.newImage('assets-1/dungeon/floor/hole.png')
   wall = love.graphics.newImage('assets-1/dungeon/wall/catacombs_0.png')
+
   mapTemplate = {
-    {wall, wall, wall, wall, wall, wall, wall, wall, wall,},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, hole, hole,},
-    {cobalt, cobalt, hole, cobalt, cobalt, cobalt, cobalt, hole, hole,},
-    {cobalt, cobalt, hole, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, hole, cobalt, cobalt, cobalt,},
-    {cobalt, hole, hole, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
-    {cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight,},
-    {cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft,},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,}, 
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
+    {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall,},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, hole, hole, wall,},
+    {wall, cobalt, cobalt, hole, cobalt, cobalt, cobalt, cobalt, hole, hole, wall,},
+    {wall, cobalt, cobalt, hole, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall,},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, hole, cobalt, cobalt, cobalt, wall,},
+    {wall, cobalt, hole, hole, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall,},
+    {wall, cobaltMirrorRightT, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, cobaltMirrorRight, wall,},
+    {wall, cobaltMirrorLeftT, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, cobaltMirrorLeft, wall,},
+    {wall, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, wall,},
+    {wall, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, wall,},
+    {wall, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, wall,},
+    {wall, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, wall,}, 
+    {wall, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, wall,},
+    {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall,},
   }
   map = Map:new(mapTemplate)
 end
@@ -47,10 +58,13 @@ function love.update(dt)
     y = y - 4
     mirrory = mirrory - 4
   end
+  cam:setPosition(x, y)
 end
 
 function love.draw()
-  love.graphics.print('Hello, world!', 0, 0)
+  cam:draw(function(camx, camy)
+  map:draw()
   love.graphics.draw(playerImg, x, y)
   love.graphics.draw(mirrorPlayerImg, mirrorx, mirrory)
+  end)
 end
