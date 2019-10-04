6cd2ca76bfe035c7b9ada591c838471b5c2b2cde
Travis <dtrode59@harrisonburg.k12.va.us>
2019-10-04 09:48:58 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,23 +7,23 @@ function love.load()
 
 function love.update(dt)
   if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
-    x = x + 3
+    x = x + 6
   end
   if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
-    y = y - 3
+    y = y - 6
   end
   if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
-    y = y + 3
+    y = y + 6
   end
   if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
-    x = x - 3
+    x = x - 6
   end
 end
 
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
+  cage = love.graphics.newImage('assets/dungeon/floor/cage')
   mapTemplate = {
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
+    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},     
     {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
     {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
     {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
