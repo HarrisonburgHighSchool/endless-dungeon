b454d482a240e375918e7848837605ce67d708fb
Edwin <exgali19@harrisonburg.k12.va.us>
2019-10-04 14:29:04 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,12 +3,40 @@ local Map = require 'core/map'
 function love.load()
   x = 370
   y = 270
+playerImg = love.graphics.newImage('assets-1/player/base/gargoyle_male.png')
   floorTile = love.graphics.newImage('assets-2/dc-dngn/floor/crystal_floor5.png')
   --wall     = love.graphics.newImage('altar-asset.png')
   template = { --a 3 x 3 map with the altar texture in the middle
-               {floorTile, floorTile, floorTile},
-               {floorTile, floorTile, floorTile},
-               {floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
              }
   map = Map:new(template)
 end
@@ -16,21 +44,21 @@ end
 
 function love.update(dt)
  if love.keyboard.isDown('up') then
-    y = y + -1
+    y = y + -5
   end
   if love.keyboard.isDown('down') then
-     y = y + 1
+     y = y + 5
    end
    if love.keyboard.isDown('left') then
-      x = x + -1
+      x = x + -5
     end
     if love.keyboard.isDown('right') then
-       x = x + 1
+       x = x + 5
      end
 end
 
 function love.draw()
   map:draw()
   love.graphics.print('Yo!', 0, 0)
-  --love.graphics.draw(playerImg, x, y)
+  love.graphics.draw(playerImg, x, y)
 end
