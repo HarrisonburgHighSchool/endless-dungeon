019794fac817d0aabaa1f491bfa3d414a3b25b76
Robi <rahail79@iMac714-462.local>
2019-10-04 14:30:29 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local Map = require 'core/map'
-
+local gamera = require 'core/gamera'
 function love.load()
   x = 400
   y = 300
@@ -26,41 +26,52 @@ function love.load()
                  {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
                }
     map = Map:new(template)
-
+cam = gamera.new(-100, -100, 2000, 2000)
 
 end
 
 function love.update(dt)
+cam:setPosition(x,y)
+cam:setPosition(a,b)
  if love.keyboard.isDown('up')then
  y = y - 5
+ cam:setPosition(x,y)
  end
  if love.keyboard.isDown('down')then
  y = y + 5
+ cam:setPosition(x,y)
  end
  if love.keyboard.isDown('right')then
  x = x + 5
+ cam:setPosition(x,y)
 end
  if love.keyboard.isDown('left')then
  x = x - 5
+ cam:setPosition(x,y)
  end
 
  if love.keyboard.isDown('w')then
  b = b - 5
+ cam:setPosition(a,b)
  end
  if love.keyboard.isDown('s')then
- b = b + 5
+ b = b + 50
+ cam:setPosition(a,b)
  end
  if love.keyboard.isDown('d')then
  a = a + 5
+ cam:setPosition(a,b)
 end
  if love.keyboard.isDown('a')then
  a = a - 5
+ cam:setPosition(a,b)
  end
  end
 function love.draw()
+  cam:draw(function(l, t, w, h)
    map:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
   love.graphics.draw(playerImg1, a, b)
-
+end)
 end
