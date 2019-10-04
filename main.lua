e24d24a91d45ceaa5d01cd77a270836c6939a41a
Corey <cdrupe35@harrisonburg.k12.va.us>
2019-10-04 09:50:52 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,25 +4,31 @@ function love.load()
 enter = 1
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/monster/download.png')
+  playerImg = love.graphics.newImage('assets-1/player/base/gargoyle_male.png')
 
   floor = love.graphics.newImage('assets-1/dungeon/floor/cobble_blood_2.png')
-  floor1 = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
-  floor2 = love.graphics.newImage('assets-1/dungeon/floor/cobble_blood_12.png')
+  floor1 = love.graphics.newImage('assets-1/dungeon/floor/cobble_blood_12.png')
+  floor2 = love.graphics.newImage('assets-1/dungeon/floor/cobble_blood_6.png')
+  wall = love.graphics.newImage('assets-1/dungeon/wall/catacombs_0.png')
+  wall1 = love.graphics.newImage('assets-1/dungeon/wall/catacombs_3.png')
+  wall2 = love.graphics.newImage('assets-1/dungeon/wall/catacombs_7.png')
+  wall3 = love.graphics.newImage('assets-1/dungeon/wall/catacombs_5.png')
+  wall_enter = love.graphics.newImage('assets-1/dungeon/wall/catacombs_12.png')
+ 
   template = { 
-               {floor1, floor1, floor1, floor1, floor, floor1, floor1, floor1, floor1},
-               {floor1, floor2, floor2, floor, floor, floor, floor2, floor2, floor1},
-               {floor1, floor2, floor, floor, floor, floor, floor, floor2, floor1},
-               {floor1, floor, floor2, floor, floor, floor, floor2, floor, floor1},
-               {floor1, floor, floor, floor, floor, floor, floor, floor, floor1},
-               {floor1, floor, floor, floor2, floor, floor2, floor, floor, floor1},
-               {floor1, floor, floor, floor, floor, floor2, floor, floor, floor1},
-               {floor1, floor, floor2, floor, floor, floor, floor2, floor, floor1},
-               {floor1, floor, floor, floor, floor2, floor, floor, floor, floor1},
-               {floor1, floor, floor, floor, floor, floor, floor, floor, floor1},
-               {floor1, floor2, floor, floor, floor, floor, floor, floor2, floor1},
-               {floor1, floor2, floor2, floor, floor, floor, floor2, floor2, floor1},
-               {floor1, floor1, floor1, floor1, floor1, floor1, floor1, floor1, floor1},
+               {wall, wall1, wall3, wall1, wall3, wall2, wall3, wall, wall},
+               {wall1, floor1, floor1, floor, floor, floor, floor1, floor1, wall},
+               {wall, floor1, floor, floor, floor, floor, floor, floor1, wall},
+               {wall3, floor, floor1, floor, floor, floor, floor1, floor, wall1},
+               {wall1, floor, floor, floor, floor, floor, floor, floor, wall},
+               {wall3, floor, floor, floor1, floor, floor1, floor, floor, wall3},
+               {wall_enter, floor, floor, floor, floor, floor1, floor, floor, wall1},
+               {wall2, floor, floor1, floor, floor, floor, floor1, floor, wall},
+               {wall, floor, floor, floor, floor1, floor, floor, floor, wall1},
+               {wall1, floor, floor, floor, floor, floor, floor, floor, wall2},
+               {wall3, floor1, floor, floor, floor, floor, floor, floor1, wall1},
+               {wall, floor1, floor1, floor, floor, floor, floor1, floor2, wall3},
+               {wall3, wall1, wall1, wall3, wall1, wall, wall, wall, wall},
              }
     map = Map:new(template)
 end
@@ -41,16 +47,16 @@ end
     y = y + 3
 end
 if(x < 62)then 
-    x = x + 4
+    x = x + 3
 end
-if(x > 730)then 
-  x = x - 4
+if(x > 700)then 
+  x = x - 3
 end
 if(y < 62)then 
-  y = y + 4
+  y = y + 3
 end
-if(y > 480)then 
-  y = y - 4
+if(y > 450)then 
+  y = y - 3
 end
 end
 
