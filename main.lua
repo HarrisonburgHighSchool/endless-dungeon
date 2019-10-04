04eefdaf472378c018b4990d7fa74c61245189f6
Harrison Dailey <hfdail79>
2019-10-04 09:50:58 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,23 @@
+local Map = require 'core/map'
 
 function love.load()
   
   wall = love.graphics.newImage('assets-1/dungeon/wall/catacombs_1.png')
   floor = love.graphics.newImage('assets-1/dungeon/floor/crypt_10.png')
-  door = love.graphics.newImage('assets-1/dungeon/doors/runed_door.png')
+  
   template = {
     {wall, wall, wall, wall, wall, wall, wall, wall, wall},
     {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, wall, wall, wall, wall, wall, wall, wall, wall},}
+    {wall, floor, wall, wall, wall, wall, wall, floor, wall},
+    {wall, floor, wall, floor, floor, floor, wall, floor, wall},
+    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
+    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
+    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
+    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
+    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
+    {wall, floor, wall, wall, wall, floor, wall, floor, wall},
+    {wall, floor, floor, floor, floor, floor, wall, floor, wall},
+    {wall, wall, wall, wall, wall, wall, wall, floor, wall}}
   map = Map:new(template)
   x = 400
   y = 300
@@ -25,17 +26,17 @@ end
 
 
 function love.update(dt)
-  if love.keyboard.isDown('up') thens
-  y=y-1
+  if love.keyboard.isDown('up') then
+  y=y-100
   end
   if love.keyboard.isDown('down') then
-  y=y+1
+  y=y+100
   end
   if love.keyboard.isDown('right') then
-  x=x+1
+  x=x+100
   end
   if love.keyboard.isDown('left') then
-  x=x-1
+  x=x-100
   end
 
   
@@ -43,7 +44,6 @@ end
 
 
 function love.draw()
-  
   map:draw()
 
   love.graphics.draw(playerImg, x, y)
