2446e1fb62695525f255ce2909933494cfa6993e
Daniel V <davarg@harrisonburg.k12.va.us>
2019-10-04 09:52:50 -0400

    Beta V0.0.4

--- main.lua
+++ main.lua
@@ -3,22 +3,27 @@ function love.load()
   map = Map:new(20,20)
   x = 400
   y = 300
+  --playerImg = love.graphics.newImage('a/images.png')
   playerImg = love.graphics.newImage('assets-1/player/transform/dragon_form_red.png')
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
+  vgate = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_middle.png')
+  vgate1 = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_up.png')
+  vgate2 = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_down.png')
+  wall1 = love.graphics.newImage('assets-1/dungeon/wall/lab-metal_0.png') 
   template = {
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
-    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, cobalt},
+    {wall1, wall1, wall1,cobalt,cobalt,cobalt, wall1,wall1, wall1, wall1},
+    {wall1, cobalt, wall1,  vgate1, vgate, vgate2, wall1,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
+    {wall1,wall1, wall1, wall1, wall1, wall1, wall1,wall1, wall1, wall1},
   }
 
   map = Map:new(template)
