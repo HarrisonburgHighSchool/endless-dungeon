248e8ebf7e483efb02a89aa17ecde933bc62c2f1
Chase baker <cmbake99@harrisonburg.k12.va.us>
2019-10-04 08:23:57 -0400

    dsfgyu

--- main.lua
+++ main.lua
@@ -1,23 +1,30 @@
 local Map = require 'core/map'
+local gamera = require 'core/gamera'
 love.graphics.setDefaultFilter('nearest', 'nearest')
 function love.load()
+  cam = gamera.new(-500, -500, 2000, 2000)
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  playerImg = love.graphics.newImage('assets-1/monster/human.png')
   a = 500
   b = 400
-  playerImg2 = love.graphics.newImage('assets-1/monster/human.png')
+  playerImg2 = love.graphics.newImage('assets-1/monster/ironbrand_convoker.png')
   tile = love.graphics.newImage('assets-1/dungeon/floor/sand_6.png')
-  path = love.graphics.newImage('assets-1/dungeon/floor/lab-stone_0.png')
+  path = love.graphics.newImage('assets-1/dungeon/wall/lab-stone_0.png')
 
 
   template = { --a 3 x 3 map with the altar texture in the middle
-                 {tile, path, tile},
-                 {tile, path, tile},
-                 {tile, path, tile},
+                 {tile, tile, path, tile, tile},
+                 {tile, tile, path, tile, tile},
+                 {tile, tile, path, tile, tile},
+                 {tile, tile, path, tile, tile},
+                 {tile, tile, path, tile, tile},
+                 {tile, tile, path, tile, tile},
+                 {tile, tile, path, tile, tile},
                }
   map = Map:new(template)
-  map:changeScale(5)
+  map:changeScale(2)
+   
 end
 
 function love.update(dt)
@@ -45,11 +52,15 @@ function love.update(dt)
   if love.keyboard.isDown('w') then
     b = b - 1
   end
+  cam:setPosition(x, y)
 end
 
 function love.draw()
+  cam:draw(function(l, t, w, h)
   map:draw()
   love.graphics.print('AD 117, the peak of the Roman Empires might.', 0, 0)
   love.graphics.draw(playerImg, x, y)
   love.graphics.draw(playerImg2, a, b)
+    --Draw everything here. For example:
+  end)
 end
