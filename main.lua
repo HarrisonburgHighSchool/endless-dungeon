8a35839c7a3593c2cfc283db3c618ba8bef62600
Aidan Saunders <adsaun19@harrisonburg.k12.va.us>
2019-10-04 14:32:06 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 
 local Map = require 'core/map'
+local gamera = require 'core/gamera'
 
 function love.load()
+  cam = gamera.new(0, 0, 2000, 600) -- Create a camera that can move in a rectangle from 0, 0 to 2000, 2000
   x = 1
   y = 235
   playerImg = love.graphics.newImage('assets-1/player/base/lorc_male_6.png')
 floorTile = love.graphics.newImage('assets-1/dungeon/floor/sand_1.png')
-path = love.graphics.newImage('assets-1/dungeon/floor/mud_0.png')
+path = love.graphics.newImage('assets-2/dc-dngn/floor/swamp0.png')
 floor = {
                {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
                {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
@@ -24,7 +26,24 @@ floor = {
                {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
                {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
                {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, path, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
              }
+
   map = Map:new(floor)
 end
 
@@ -41,10 +60,15 @@ function love.update(dt)
   if love.keyboard.isDown('down')then
     y = y + 3
   end
+  cam:setPosition(x, y)
 end
 
 function love.draw()
-  map:draw()
-  love.graphics.print('Hello, world!', 0, 0)
-  love.graphics.draw(playerImg, x, y)
+  cam:draw(function(l, t, w, h)
+
+    map:draw()
+    love.graphics.print('Hello, world!', 0, 0)
+    love.graphics.draw(playerImg, x, y)
+
+  end)
 end
