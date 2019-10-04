44e86e23fae4f8d434e71c6b722fc845ce427024
Edwin <exgali19@harrisonburg.k12.va.us>
2019-10-04 14:04:28 -0400

    yo

--- main.lua
+++ main.lua
@@ -1,8 +1,18 @@
+local Map = require 'core/map'
+
 function love.load()
   x = 370
   y = 270
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  floorTile = love.graphics.newImage('assets-2/dc-dngn/floor/crystal_floor5.png')
+  --wall     = love.graphics.newImage('altar-asset.png')
+  template = { --a 3 x 3 map with the altar texture in the middle
+               {floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile},
+             }
+  map = Map:new(template)
 end
+
 
 function love.update(dt)
  if love.keyboard.isDown('up') then
@@ -20,6 +30,7 @@ function love.update(dt)
 end
 
 function love.draw()
+  map:draw()
   love.graphics.print('Yo!', 0, 0)
-  love.graphics.draw(playerImg, x, y)
+  --love.graphics.draw(playerImg, x, y)
 end
