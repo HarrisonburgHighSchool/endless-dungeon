c9569bc5054bbe4a6d1ada3e37e183340e831dbc
xander <ximart79@harrisonburg.k12.va.us>
2019-10-04 09:54:41 -0400

    not done

--- main.lua
+++ main.lua
@@ -5,6 +5,14 @@ function love.load()
   x = 400
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  floorTile = love.graphics.newImage('assets-1/dungeon/floor/etched_5.png')
+  altar     = love.graphics.newImage('assets-1/dungeon/floor/bog_green1.png')
+  template = { --a 3 x 3 map with the altar texture in the middle
+                 {floorTile, floorTile, floorTile},
+                 {floorTile, altar, floorTile},
+                 {floorTile, floorTile, floorTile},
+  }
+
 end
 
 function love.update(dt)
@@ -21,6 +29,8 @@ if love.keyboard.isDown('down') then
     y = y + 5
 end
 end
+
+
 
 function love.draw()
   map:draw()
