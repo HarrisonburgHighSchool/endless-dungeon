45f27a2e1029184c68ec6c7144e6ef417e3ece83
manny <exsosa39@harrisonburg.k12.va.us>
2019-10-04 13:48:36 -0400

    dsgv

--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@ function love.load()
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
 end
-
 function love.update(dt)
   if love.keyboard.isDown("left") then
         x = x - 4
@@ -21,8 +20,8 @@ function love.update(dt)
        end
        function love.load()
 
-  floorTile = love.graphics.newImage('asset.png')
-  altar     = love.graphics.newImage('altar-asset.png')
+  floorTile = love.graphics.newImage('dirt_east.png')
+  altar     = love.graphics.newImage('dirt_east.png')
   template = { --a 3 x 3 map with the altar texture in the middle
                {floorTile, floorTile, floorTile},
                {floorTile, altar, floorTile},
@@ -31,6 +30,6 @@ function love.update(dt)
   map = Map:new(template)
 end
 function love.draw()
-  love.graphics.print('mike hawk!', 0, 0)
+  love.graphics.print('play here !', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
