1d542a19ea2ea49e1713f7fac86a48378ba6b941
Jacob <joyear62@harrisonburg.k12.va.us>
2019-10-04 09:48:40 -0400

    dngn-game

--- main.lua
+++ main.lua
@@ -1,59 +1,49 @@
+local Map = require 'core/map'
+
 love.graphics.setDefaultFilter('nearest', 'nearest')
 function love.load()
   x = 50
   y = 285
-  crystalFloor = love.graphics.newImage('assets-2/dc-dngn/floor/cobble_blood4.png')
+  rectFloor = love.graphics.newImage('assets-1/dungeon/floor/rect_gray_0.png')
+  rect1Floor = love.graphics.newImage('assets-1/dungeon/floor/rect_gray_1.png')
+  rect2Floor = love.graphics.newImage('assets-1/dungeon/floor/rect_gray_2.png')
+  rect3Floor = love.graphics.newImage('assets-1/dungeon/floor/rect_gray_3.png')
   doorway = love.graphics.newImage('assets-2/dc-dngn/gateways/dngn_enter_dis.png')
-  altar1 = love.graphics.newImage('assets-2/dc-dngn/altars/dngn_altar_xom3.png')
-  altar2 = love.graphics.newImage('assets-2/dc-dngn/altars/dngn_altar_xom1.png')
-  altar3 = love.graphics.newImage('assets-2/dc-dngn/altars/dngn_altar_xom2.png')
-  altar4 = love.graphics.newImage('assets-2/dc-dngn/altars/dngn_altar_xom4.png')
+  cobaltWall = love.graphics.newImage('assets-1/dungeon/wall/cobalt_stone_1.png')
   axeTrap = love.graphics.newImage('assets-2/dc-dngn/dngn_trap_axe.png')
   shaftTrap = love.graphics.newImage('assets-2/dc-dngn/dngn_trap_shaft.png')
   playerImg = love.graphics.newImage('assets-1/monster/juggernaut.png')
 
   template = { --a 3 x 3 map with the altar texture in the middle
-  {altar1, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, altar4},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap, axeTrap},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {doorway, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, crystalFloor, doorway},
-  {altar2, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, doorway, altar3},
+ 
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor},
 }
   map = Map:new(template)
 end
--- DELETE THIS
 
 function love.update(dt)
-  if love.keyboard.isDown('w') and y > 0 then   -- if the 'up' key is being pressed
+  if love.keyboard.isDown('w') and y > 0 then   -- if the 'w' key is being pressed
     y = y - 2
   end
-  if love.keyboard.isDown('s') and y < 569 then   -- if the 'up' key is being pressed
-    y = y + 2
-  end
-  if love.keyboard.isDown('a') and x > 0 then   -- if the 'up' key is being pressed
+  if love.keyboard.isDown('a') and x > 0 then   -- if the 'a' key is being pressed
     x = x - 2
   end
-  if love.keyboard.isDown('d') and x < 769 then   -- if the 'up' key is being pressed
+  if love.keyboard.isDown('s') and y < 569 then   -- if the 's' key is being pressed
+    y = y + 2
+  end
+  if love.keyboard.isDown('d') and x < 769 then   -- if the 'd' key is being pressed
     x = x + 2
   end
 end
