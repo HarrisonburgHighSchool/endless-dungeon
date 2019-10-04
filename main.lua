ededf74fb83448b61399986d7d1a7c5694c870ad
Precious <kpcast48@harrisonburg.k12.va.us>
2019-10-04 09:50:42 -0400

    grass
    grass

--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
+local Map = require 'core/map'
 love.graphics.setDefaultFilter('nearest', 'nearest')
 function love.load()
   x = 400
   y = 300
   playerImg = love.graphics.newImage('assets-2/dc-mon/glowing_shapeshifter.png')
+  secretpath = love.graphics.newImage('assets-2/dc-dngn/gateways/stone_stairs_down.png')
   statue = love.graphics.newImage('assets-1/dungeon/statues/statue_angel.png')
+  grass = love.graphics.newImage('assets-2/dc-dngn/floor/grass/grass_flowers_yellow1.png')
   limestone = love.graphics.newImage('assets-1/dungeon/floor/limestone_1.png')
-  
+
   mapTemplate = {
     {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
     {limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone,limestone},
@@ -28,6 +31,7 @@ end
 
 
 
+
 function love.update(dt)
   if love.keyboard.isDown('down') then   -- if the 'up' key is being pressed...
     y = y + 1
@@ -46,7 +50,9 @@ end
 function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   map:draw()
-  love.graphics.draw(statue, 400, 270)
+  love.graphics.draw(grass, 500, 270, 0, 2)
+  love.graphics.draw(statue, 300, 400, 0, 2)
+  love.graphics.draw(secretpath, 400, 500)
   love.graphics.draw(playerImg, x, y)
  
   
