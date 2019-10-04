921a97551ea3dbe20d933524cc6ffafb0867fe65
Chase baker <cmbake99@harrisonburg.k12.va.us>
2019-10-04 14:29:01 -0400

    rhre

--- main.lua
+++ main.lua
@@ -2,25 +2,38 @@ local Map = require 'core/map'
 local gamera = require 'core/gamera'
 love.graphics.setDefaultFilter('nearest', 'nearest')
 function love.load()
-  cam = gamera.new(-500, -500, 2000, 2000)
-  x = 400
-  y = 300
+  cam = gamera.new(0, 0, 1000, 1000)
   playerImg = love.graphics.newImage('assets-1/monster/human.png')
-  a = 500
-  b = 400
-  playerImg2 = love.graphics.newImage('assets-1/monster/ironbrand_convoker.png')
+  x = 400
+  y = 550
+  enemy = love.graphics.newImage('assets-1/monster/ironheart_preserver.png')
+  a = 560
+  h = 550
+  enemy2 = love.graphics.newImage('assets-1/monster/ironbrand_convoker.png')
+  c = 600
+  d = 600
+  enemy3 = love.graphics.newImage('assets-1/monster/ironbrand_convoker.png')
+  e = 600
+  f = 500
+  questItem = love.graphics.newImage('assets-1/item/amulet/artefact/urand_vitality.png')
+  g = 675
+  b = 550
   tile = love.graphics.newImage('assets-1/dungeon/floor/sand_6.png')
   path = love.graphics.newImage('assets-1/dungeon/wall/lab-stone_0.png')
 
 
   template = { --a 3 x 3 map with the altar texture in the middle
-                 {tile, tile, path, tile, tile},
-                 {tile, tile, path, tile, tile},
-                 {tile, tile, path, tile, tile},
-                 {tile, tile, path, tile, tile},
-                 {tile, tile, path, tile, tile},
-                 {tile, tile, path, tile, tile},
-                 {tile, tile, path, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
+                 {tile, tile, tile, tile, path, tile, tile, tile, tile},
                }
   map = Map:new(template)
   map:changeScale(2)
@@ -47,10 +60,10 @@ function love.update(dt)
     a = a - 1
   end
   if love.keyboard.isDown('s') then
-    b = b + 1
+    h = h + 1
   end
   if love.keyboard.isDown('w') then
-    b = b - 1
+    h = h - 1
   end
   cam:setPosition(x, y)
 end
@@ -60,7 +73,10 @@ function love.draw()
   map:draw()
   love.graphics.print('AD 117, the peak of the Roman Empires might.', 0, 0)
   love.graphics.draw(playerImg, x, y)
-  love.graphics.draw(playerImg2, a, b)
+  love.graphics.draw(enemy, a, b)
+  love.graphics.draw(enemy2, c, d)
+  love.graphics.draw(enemy3, e, f)
+  love.graphics.draw(questItem, g, b)
     --Draw everything here. For example:
   end)
 end
