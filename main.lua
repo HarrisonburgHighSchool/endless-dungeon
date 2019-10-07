03d8799c8c612645d7534c512851d7cf019745f0
Brian <besont40@harrisonburg.k12.va.us>
2019-10-07 09:47:02 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,32 +1,36 @@
 local Map = require 'core/map'
 local gamera = require 'core/gamera'
+local Util = require 'core/util'
 
 function love.load()
   cam = gamera.new(0,0,2000,2000)
   x = 400
   y = 300
-  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  playerImg = love.graphics.newImage('assets-1/monster/demons/hellion.png')
+  w = 64   -- The player's width is 64
+  h = 64   -- The player's height is 64
+  hp = 100 -- Set the player's HP to 100 at the start of the game
   floorTile = love.graphics.newImage('assets-1/dungeon/floor/etched_5.png')
-  altar     = love.graphics.newImage('assets-1/monster/demons/hellion.png')
+  altar     = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_3.png')
   template = { --a 20 x 20 map with the altar texture in the middle
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
-                {floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},{floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, altar,altar,altar,altar,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, altar,altar,altar,altar,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, altar,altar,altar,altar,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, altar,altar,altar,altar,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, altar,altar,altar,altar,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile},
+                
 
 
 
@@ -37,20 +41,27 @@ end
 
 function love.update(dt)
   -- Nothing update
-if love.keyboard.isDown('w') then   -- if the 'up' key is being pressed...
-  y = y - 5
-end
-if love.keyboard.isDown('d') then   -- if the 'up' key is being pressed...
-  x = x + 5
-end
-if love.keyboard.isDown('s') then   -- if the 'up' key is being pressed...
-  y = y + 5
-end
-if love.keyboard.isDown('a') then   -- if the 'up' key is being pressed...
-  x = x - 5
-  
-end
-cam:setPosition(x,y)
+ -- Set up player movement
+ if love.keyboard.isDown('up') then
+  y = y - 1
+  end
+  if love.keyboard.isDown('down') then
+    y = y + 1
+  end
+  if love.keyboard.isDown('left') then
+    x = x - 1
+  end
+  if love.keyboard.isDown('right') then
+    x = x + 1
+  end
+  cam:setPosition(x,y)
+
+
+
+  if cc(x, y, w, h,   0, 0, 64, 64) then  
+    -- if true, decrease HP:
+    hp = hp - 1
+  end
 end
 
 function love.draw()
@@ -58,5 +69,7 @@ function love.draw()
     map:draw()
     love.graphics.print('Hi how has your day!', 0, 0)
     love.graphics.draw(playerImg, x, y)
+    love.graphics.rectangle('line', 0, 0, 64, 64)
+    love.graphics.print(hp, 0, 0)
 end)
 end
