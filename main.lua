9087178c71ad139d0c5529c990f4a5559994c926
Daniel V <davarg@harrisonburg.k12.va.us>
2019-10-07 09:47:45 -0400

    Beta V0.0.5

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 local Map = require 'core/map'
+local Util = require 'core/util'
 --love.graphics.setDefaultFilter('nearest', 'nearest')
 function love.load()
   map = Map:new(20,20)
@@ -6,25 +7,28 @@ function love.load()
   y = 300
   --playerImg = love.graphics.newImage('a/images.png')
   playerImg = love.graphics.newImage('assets-1/player/transform/dragon_form_red.png')
+  w = 64
+  h = 64
+  hp = 100
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
   vgate = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_middle.png')
   vgate1 = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_up.png')
   vgate2 = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_down.png')
-  wall1 = love.graphics.newImage('assets-1/dungeon/wall/lab-metal_0.png') 
+  wall = love.graphics.newImage('assets-1/dungeon/wall/lab-metal_0.png') 
   template = {
-    {wall1, wall1, wall1,cobalt,cobalt,cobalt, wall1,wall1, wall1, wall1},
-    {wall1, cobalt, wall1,  vgate1, vgate, vgate2, wall1,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall1},
-    {wall1,wall1, wall1, wall1, wall1, wall1, wall1,wall1, wall1, wall1},
+    {wall, wall, wall,vgate1, vgate, vgate2, wall, wall, wall, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall,wall, wall, wall, wall, wall, wall,wall, wall, wall},
   }
 
   map = Map:new(template)
@@ -44,8 +48,10 @@ function love.update(dt)
   if love.keyboard.isDown('w') then   -- if the 'up' key is being pressed...
     y = y - 6
   end
+if cc(x, y, w, h,   0, 0, 4000, 3000) then  
+  hp = hp - 1
+  end
 end
-
 --function love.update(dt)
   -- Nothing to update yet
 --end
@@ -53,4 +59,6 @@ end
 function love.draw()
   map:draw()
   love.graphics.draw(playerImg, x, y)
+  love.graphics.rectangle('line', 0, 0, 64, 64)
+  love.graphics.print(hp, 0, 0)
 end
