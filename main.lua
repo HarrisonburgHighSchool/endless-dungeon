a8080410b09bed172ce82fdb06b35f80a53eeb76
Robi <rahail79@iMac714-462.local>
2019-10-07 14:23:52 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,15 @@
 local Map = require 'core/map'
 local gamera = require 'core/gamera'
+local Util = require 'core/util'
 function love.load()
   x = 400
   y = 300
   a = 400
   b = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  w = 64   -- The player's width is 64
+  h = 64   -- The player's height is 64
+  hp = 100 -- Set the player's HP to 100 at the start of the game
   playerImg1 = love.graphics.newImage('assets-1/player/base/tengu_wingless_brown_male.png')
 
     floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_5.png')
@@ -66,6 +70,12 @@ end
  a = a - 5
  cam:setPosition(a,b)
  end
+
+ if cc(x, y, w, h,   0, 0, 64, 64) then
+     -- if true, decrease HP:
+     hp = hp - 1
+   end
+
  end
 function love.draw()
   cam:draw(function(l, t, w, h)
@@ -73,5 +83,6 @@ function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
   love.graphics.draw(playerImg1, a, b)
+  love.graphics.print(hp, 0, 20)
 end)
 end
