da29ff7b5914a5b1b9b037382351747a29fd6ec5
nibala45 <nabala45@harrisonburg.k112.va.us>
2019-10-07 09:35:52 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,12 @@
 local Map = require 'core/map'
 playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
-playerImg = love.graphics.draw('assets-1/dungeon/floor/black_cobalt_12.png')
 function love.load()
   x = 400
   y = 300
 end
 function love.update(dt)
   if love.keyboard.isDown('right')then -- if the 'up' key is being pressed
-    x = x + 10
+    x = x + 15
   end
   if love.keyboard.isDown('left')then -- if the 'up' key is being pressed
     x = x - 15
@@ -22,8 +21,9 @@ end
 
 function love.draw()
   love.graphics.print('Hello, world!', 10, 10)
-  love.graphics.draw('black_cobalt_12.png', x, y)
+  playerImg = love.graphics.draw('assets-1/dungeon/floor/black_cobalt_12.png')
   love.graphics.draw(playerImg, 0, 0)
+  love.graphics.draw('assets-1/player/base/octopode_1.png')
   map:draw()
 end
 map = Map:new(10, 10) -- Create a 10 x 10 map object named "map"
