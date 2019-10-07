ef802e41d50eb0fc119c488f213ac1f184988453
Travis <dtrode59@harrisonburg.k12.va.us>
2019-10-07 09:45:43 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,54 +1,51 @@
 love.graphics.setDefaultFilter('nearest', 'nearest')
+
+local Map = require 'core/map'
+
 function love.load()
   x = 0
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
-
-function love.update(dt)
-  if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
-    x = x + 6
-  end
-  if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
-    y = y - 6
-  end
-  if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
-    y = y + 6
-  end
-  if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
-    x = x - 6
-  end
-end
-
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
-  cage = love.graphics.newImage('assets/dungeon/floor/cage')
+  cage = love.graphics.newImage('assets-1/dungeon/floor/cage_0.png')
   mapTemplate = {
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},     
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
-    {cobalt, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cobalt},
+    {cage, cage,cage, cage,cage,cage,cage,cage,cage,cage,cage,cage,cage,cage,cage,cage,cage},     
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
+    {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
   }
 
   map = Map:new(mapTemplate)
 end
 
+
+
 function love.update(dt)
   if love.keyboard.isDown('right') then
-    x = x + 1
+    x = x + 6
   end
-end
+    if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
+      y = y - 6
+    end
+    if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
+      y = y  + 6
+    end
+    if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
+      x = x - 6
+    end
+  end
 
 function love.draw()
+  map:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
