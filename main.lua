c0ba0b08316add94be3c87a9f96b45a2216312f1
Corey <ctbesh99@harrisonburg.k12.va.us>
2019-10-07 09:45:49 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,13 +4,17 @@ local Map = require 'core/map'
 
 function love.load()
   collide1 = false
-  collide2 = false
   i = 1
  bossdefeated = 1
-  x = 400
-  y = 300
-  px = 100
-  py = 100
+ x = 300
+ y = 400
+  x2 = 193
+  y2 = 192
+  w2 = 192
+  h2 = 192
+ w = 64
+ h = 64
+ HP = 100
   playerImg = love.graphics.newImage('assets-1/monster/aquatic/Sonic.png')
   cobble = love.graphics.newImage('assets-1/dungeon/floor/cobble_blood_1.png')
   colbalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_12.png')
@@ -36,33 +40,22 @@ end
 
 
 function love.update(dt)
-  if love.keyboard.isDown('right') then   -- if the 'up' key is being pressed...
+  if love.keyboard.isDown('right') then  
     x = x + 3
    
   end
-    if love.keyboard.isDown('left') then   -- if the 'up' key is being pressed...
+    if love.keyboard.isDown('left') then  
       x = x - 3
   end
-  if love.keyboard.isDown('down') then   -- if the 'up' key is being pressed...
+  if love.keyboard.isDown('down') then 
     y = y + 3
 end
-if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
+if love.keyboard.isDown('up') then
   y = y - 3
 end
 
-if love.keyboard.isDown('d') then   -- if the 'up' key is being pressed...
-  px = px + 3
- 
-end
-  if love.keyboard.isDown('a') then   -- if the 'up' key is being pressed...
-    px = px - 3
-end
-if love.keyboard.isDown('s') then   -- if the 'up' key is being pressed...
-  py = py + 3
-end
-if love.keyboard.isDown('w') then   -- if the 'up' key is being pressed...
-py = py - 3
-end
+
+
 if(x > 640) then
   x = x - 3
 end
@@ -76,9 +69,19 @@ if(y < 60) then
   y = y + 3
 end
 
-collide1 = collision(x, y, 190, 190, 328-190, 320-190)
-collide2 = collision(px, py, 190, 190, 328-190, 320-190)
+if(x + w > x2 and y + h > y2 and y2 + h2 > y and x2 + w2 > x) then
+  collide1 = true
 
+else
+
+  collide1 = false
+end
+if(collide1 == true) then
+  HP = HP - 0.1
+end
+if(HP < 0) then
+  HP = 0
+end
 if(y == 260 and x == 50) then
   x = x + 0
   i = 2
@@ -92,20 +95,19 @@ end
 
 function love.draw()
   map:draw()
-  love.graphics.print(tostring(collide1)..", "..tostring(collide2), 100, 0)
+  if(collide = true) then
+    love.graphics.draw(playerImg, x, y)
+
   love.graphics.draw(playerImg, x, y)
-  love.graphics.draw(playerImg, px, py)
   love.graphics.print(x, 10, 20)
   love.graphics.print(y, 30 ,30)
+  love.graphics.print(HP, 100, 100)
+  love.graphics.print(tostring(collide1), 0, 0)
 
-  love.graphics.print(x..", "..y)
   
 end
 
-function collision(x1, y1, x2, y2, w2, h2)
-if(y2 < y1 and y1 < y2 + h2 and x1 < x2 + w2 and x2 < x1 ) then
-  return true
-else
-  return false
-end
-end
+
+
+
+
