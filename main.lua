01e137c8e1dfb8a10f10e17402ceb5a8c22318ef
Jackson Calhoun <jwcalh19@harrisonburg.k12.va.us>
2019-10-07 09:45:46 -0400

    ss

--- main.lua
+++ main.lua
@@ -1,17 +1,21 @@
 local Map = require 'core/map'
 function love.load()
- 
+local Util = require 'core/util'
+
  
   fountain = love.graphics.newImage('assets-2/dc-dngn/dngn_blue_fountain2.png')
   fountain2 = love.graphics.newImage('assets-2/dc-dngn/dngn_blood_fountain.png')
   dragon = love.graphics.newImage('assets-1/dungeon/statues/statue_dragon.png')
-  
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
   floor = love.graphics.newImage('assets-1/dungeon/floor/crypt_domino_2a.png')
   wall = love.graphics.newImage('assets-2/dc-dngn/wall/brick_dark3.png')
   door = love.graphics.newImage('assets-2/dc-dngn/dngn_open_door.png')
   doorc = love.graphics.newImage('assets-2/dc-dngn/dngn_closed_door.png')
-
+  x = 65
+  y = 80
+  w = 64   -- The player's width is 64
+  h = 64   -- The player's height is 64
+  hp = 100 -- Set the player's HP to 100 at the start of the game
   template = {
     {wall, wall, wall, wall, wall, wall, wall, wall, doorc, wall},
     {door, floor, floor, floor, wall, floor, floor, floor, floor, wall},
@@ -32,17 +36,24 @@ map = Map:new(template)
 end
 
 function love.update(dt)
-  if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
-    x = x + 15
+  if cc(x, y, w, h,  0, 0, 0, 0) then
+    if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
+      x = x + 15
+    end
+    if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
+      x = x - 15
+    end
+    if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
+      y = y - 15
+    end
+    if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
+      y = y + 15
+    end
   end
-  if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
-    x = x - 15
-  end
-  if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
-    y = y - 15
-  end
-  if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
-    y = y + 15
+
+  if cc(x, y, w, h,   0, 0, 64, 64) then  
+    -- if true, decrease HP:
+    hp = hp - 1
   end
 end
 
@@ -54,5 +65,7 @@ function love.draw()
   love.graphics.draw(fountain, 675, 100)
   love.graphics.draw(fountain2, 675, 500)
   love.graphics.draw(dragon, 400, 200)
-  love.graphics.draw(playerImg, 65, 80)
+  love.graphics.draw(playerImg, x, y)
+  love.graphics.rectangle('line', 0, 0, 64, 64)
+  love.graphics.print(hp, 0, 0)
 end
