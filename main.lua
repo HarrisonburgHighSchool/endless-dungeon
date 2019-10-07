1d0af80563af4e91bf9bf6edb4b92598854dc3cd
Aidan Saunders <adsaun19@harrisonburg.k12.va.us>
2019-10-07 14:21:50 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
 
 local Map = require 'core/map'
 local gamera = require 'core/gamera'
+local Util = require 'core/util'
 
 function love.load()
   cam = gamera.new(0, 0, 2000, 600) -- Create a camera that can move in a rectangle from 0, 0 to 2000, 2000
   x = 1
   y = 235
   playerImg = love.graphics.newImage('assets-1/player/base/lorc_male_6.png')
+  w=64
+  h=64
+  hp=100
 floorTile = love.graphics.newImage('assets-1/dungeon/floor/sand_1.png')
 path = love.graphics.newImage('assets-2/dc-dngn/floor/swamp0.png')
 floor = {
@@ -45,6 +49,7 @@ floor = {
              }
 
   map = Map:new(floor)
+
 end
 
 function love.update(dt)
@@ -61,14 +66,20 @@ function love.update(dt)
     y = y + 3
   end
   cam:setPosition(x, y)
+  -- x, y, w, h all represent the player's rectangle. The other values are a rectangle in the upper corner
+ if cc(x, y, w, h,   0, 0, 64, 64) then
+   -- if true, decrease HP:
+   hp = hp - 1
+ end
 end
 
 function love.draw()
   cam:draw(function(l, t, w, h)
 
     map:draw()
-    love.graphics.print('Hello, world!', 0, 0)
     love.graphics.draw(playerImg, x, y)
+    love.graphics.rectangle('line', 0, 0, 64, 64)
+    love.graphics.print(hp, 0, 0)
 
   end)
 end
