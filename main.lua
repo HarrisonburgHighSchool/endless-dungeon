7584a390c57807de2ce0297bcec7758218332f7d
Corey <cdrupe35@harrisonburg.k12.va.us>
2019-10-08 09:51:33 -0400

    xfb

--- main.lua
+++ main.lua
@@ -2,8 +2,15 @@ local Map = require 'core/map'
 
 function love.load()
 enter = 1
+collide = false
   x = 400
   y = 300
+  x2 = 313
+  y2 = 189
+  w2 = 192
+  h2 = 192
+  w = 64
+  h = 64
   playerImg = love.graphics.newImage('assets-1/player/base/gargoyle_male.png')
 
   floor = love.graphics.newImage('assets-1/dungeon/floor/cobble_blood_2.png')
@@ -21,9 +28,9 @@ enter = 1
                {wall, floor1, floor, floor, floor, floor, floor, floor1, wall},
                {wall3, floor, floor1, floor, floor, floor, floor1, floor, wall1},
                {wall1, floor, floor, floor, floor, floor, floor, floor, wall},
-               {wall3, floor, floor, floor1, floor, floor1, floor, floor, wall3},
-               {wall_enter, floor, floor, floor, floor, floor1, floor, floor, wall1},
-               {wall2, floor, floor1, floor, floor, floor, floor1, floor, wall},
+               {wall3, floor, floor, wall1, wall, wall2, floor, floor, wall3},
+               {wall_enter, floor, floor, wall, floor, wall, floor, floor, wall1},
+               {wall2, floor, floor1, wall3, wall1, wall, floor1, floor, wall},
                {wall, floor, floor, floor, floor1, floor, floor, floor, wall1},
                {wall1, floor, floor, floor, floor, floor, floor, floor, wall2},
                {wall3, floor1, floor, floor, floor, floor, floor, floor1, wall1},
@@ -58,10 +65,18 @@ end
 if(y > 450)then 
   y = y - 3
 end
+if(x + w > x2 and y + h > y2 and y2 + h2 > y and x2 + w2 > x) then 
+  collide = true
+else
+  collide = false
+end
 end
 
 function love.draw()
   map:draw()
   love.graphics.draw(playerImg, x, y)
+  love.graphics.print(x, 2, 12)
+  love.graphics.print(y, 2, 24)
+  love.graphics.print(tostring(collide), 0, 0)
 
 end
