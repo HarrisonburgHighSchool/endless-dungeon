e0491d3093b6a6c10d55c593536c565991c86890
Harrison Dailey <hfdail79>
2019-10-08 09:51:21 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,22 +3,25 @@ local Util = require 'core/util'
 
 function love.load()
   
-  wall = love.graphics.newImage('assets-1/dungeon/wall/catacombs_1.png')
-  floor = love.graphics.newImage('assets-1/dungeon/floor/crypt_10.png')
+  w0 = love.graphics.newImage('assets-1/dungeon/wall/catacombs_0.png')
+  w1 = love.graphics.newImage('assets-1/dungeon/wall/catacombs_1.png')
+  w2 = love.graphics.newImage('assets-1/dungeon/wall/catacombs_2.png')
+  f1 = love.graphics.newImage('assets-1/dungeon/floor/cobble_blood_1.png')
   
+
   template = {
-    {wall, wall, wall, wall, wall, wall, wall, wall, wall},
-    {wall, floor, floor, floor, floor, floor, floor, floor, wall},
-    {wall, floor, wall, wall, wall, wall, wall, floor, wall},
-    {wall, floor, wall, floor, floor, floor, wall, floor, wall},
-    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
-    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
-    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
-    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
-    {wall, floor, wall, floor, wall, floor, wall, floor, wall},
-    {wall, floor, floor, floor, wall, floor, wall, floor, wall},
-    {wall, floor, floor, floor, floor, floor, wall, floor, wall},
-    {wall, wall, wall, wall, wall, wall, wall, floor, wall}}
+    {w1, w1, w1, w1, w1, w1, w1, w1, w1},
+    {w1, f1, f1, f1, f1, f1, f1, f1, w1},
+    {w1, f1, w2, w2, w2, w2, w2, f1, w1},
+    {w1, f1, w2, f1, f1, f1, w2, f1, w1},
+    {w1, f1, w2, f1, f1, f1, w2, f1, w1},
+    {w1, f1, w2, f1, w0, f1, w2, f1, w1},
+    {w1, f1, w2, f1, w0, f1, w2, f1, w1},
+    {w1, f1, w2, f1, w0, f1, w2, f1, w1},
+    {w1, f1, w2, f1, w0, f1, w2, f1, w1},
+    {w1, f1, f1, f1, f1, f1, f1, f1, w1},
+    {w1, f1, f1, f1, f1, f1, f1, f1, w1},
+    {w1, w1, w1, w1, w1, w1, w1, f1, w1}}
   map = Map:new(template)
   x = 400
   y = 300
@@ -31,19 +34,19 @@ end
 
 function love.update(dt)
   if love.keyboard.isDown('up') then
-  y=y-5
+  y=y-4
   end
   if love.keyboard.isDown('down') then
-  y=y+5
+  y=y+4
   end
   if love.keyboard.isDown('right') then
-  x=x+5
+  x=x+4
   end
   if love.keyboard.isDown('left') then
-  x=x-5
+  x=x-4
   end
 
-  if cc(x, y, w, h,   0, 0, 64, 64) then  
+  if cc(x, y, w, h, 0, 0, 64, 64) then  
     hp = hp - 1
   end
 
@@ -52,8 +55,6 @@ end
 
 function love.draw()
   map:draw()
-
-  love.graphics.rectangle('line', 0, 0, 64, 64)
 
   love.graphics.draw(playerImg, x, y)
 
