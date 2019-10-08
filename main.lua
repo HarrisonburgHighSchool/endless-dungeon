55d7a2b14068803736dd5ae3f4c0dbea5f11919e
Robi <rahail79@iMac714-462.local>
2019-10-08 14:31:32 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,21 +13,21 @@ function love.load()
   playerImg1 = love.graphics.newImage('assets-1/player/base/tengu_wingless_brown_male.png')
 
     floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_5.png')
-    altar     = love.graphics.newImage('assets-1/dungeon/floor/bog_green_1.png')
+    altar     = love.graphics.newImage('assets-1/dungeon/floor/cage_3.png')
     template = { --a 3 x 3 map with the altar texture in the middle
                  {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile,},
-                 {floorTile, altar, floorTile,floorTile, altar, floorTile,floorTile, altar, floorTile,floorTile, altar,},
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
-                 {floorTile,  altar, floorTile,floorTile,  altar, floorTile,floorTile, floorTile, floorTile,floorTile, },
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile, altar, floorTile, floorTile,floorTile, },
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
-                 {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, floorTile,floorTile, },
+                 {floorTile, altar, altar,altar, altar, altar,altar, altar, altar,floorTile, altar,},
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, altar,floorTile, },
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, altar,floorTile, },
+                 {floorTile,  altar, floorTile,floorTile,  altar, floorTile,floorTile, floorTile, altar,floorTile, },
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile, altar, floorTile, floorTile,floorTile, },
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, altar,floorTile, },
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, altar,floorTile, },
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, altar,floorTile, },
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, altar,floorTile, },
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, altar,floorTile, },
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, altar,floorTile, },
+                 {floorTile, altar, floorTile,floorTile, floorTile, floorTile,floorTile, floorTile, altar,floorTile, },
                }
     map = Map:new(template)
 cam = gamera.new(-100, -100, 2000, 2000)
@@ -35,8 +35,6 @@ cam = gamera.new(-100, -100, 2000, 2000)
 end
 
 function love.update(dt)
-cam:setPosition(x,y)
-cam:setPosition(a,b)
  if love.keyboard.isDown('up')then
  y = y - 5
  cam:setPosition(x,y)
@@ -78,6 +76,7 @@ end
 
  end
 function love.draw()
+
   cam:draw(function(l, t, w, h)
    map:draw()
   love.graphics.print('Hello, world!', 0, 0)
