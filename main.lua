638e19cef462d05d04412a29b4b72ea08a4adc69
manny <exsosa39@harrisonburg.k12.va.us>
2019-10-08 14:26:37 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,13 +3,13 @@ local Map = require 'core/map'
 function love.load()
   x = 400
   y = 100
-  playerImg = love.graphics.newImage('assets-2/player/demons/blizzard_demon.png')
+  playerImg = love.graphics.newImage('assets-1/monster/demons/balrug.png')
   floorTile = love.graphics.newImage('assets-2/dc-dngn/floor/lava2.png')
   altar     = love.graphics.newImage('assets-2/dc-dngn/floor/lava2.png')
   template = { --a 6 x 6 map with the altar texture in the middle
                {floorTile, floorTile, floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,},
                {floorTile, altar, floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,},
-              {floorTile, floorTile, floorTile, floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,},
+              {floorTile, floorTile, floorTile, floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,floorTile,},
              }
 
   map = Map:new(template)
