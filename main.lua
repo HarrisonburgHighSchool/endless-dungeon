77daed084dba6045a1763a89c5892d61b0350eb8
Jacob <joyear62@harrisonburg.k12.va.us>
2019-10-09 09:53:45 -0400

    dungeon game

--- main.lua
+++ main.lua
@@ -43,29 +43,26 @@ function love.update(dt)
   if love.keyboard.isDown('w') and y > 0 then   -- if the 'w' key is being pressed
     y = y - 2
   end
-  if love.keyboard.isDown('a') and x > 0 then   -- if the 'a' key is being pressed
+  if love.keyboard.isDown('a') and x > 64 then   -- if the 'a' key is being pressed
     x = x - 2
   end
-  if love.keyboard.isDown('s') and y < 569 then   -- if the 's' key is being pressed
+  if love.keyboard.isDown('s') and y < 482 then   -- if the 's' key is being pressed
     y = y + 2
   end
-  if love.keyboard.isDown('d') and x < 769 then   -- if the 'd' key is being pressed
+  if love.keyboard.isDown('d') and x < 677 then   -- if the 'd' key is being pressed
     x = x + 2
   end
-  -- x, y, w, h all represent the player's rectangle. The other values are a rectangle in the upper corner (LEFT WALL)
- if cc(x, y, w, h,   0, 0, 64, 640) then  
-  -- if true, decrease HP:
-  x = x + 2
+ 
+-- x, y, w, h all represent the player's rectangle. The other values are a rectangle in the upper corner
+if cc(x, y, w, h,   230, 227, 25, 32) then  
+  if love.keyboard.isDown('w') and y > 0 then   -- if the 'w' key is being pressed
+    y = y + 2
+  end
  end
- -- x, y, w, h all represent the player's rectangle. The other values are a rectangle in the upper corner (RIGHT WALL)
- if cc(x, y, w, h,   738, 0, 128, 640) then  
-  -- if true, decrease HP:
-  x = x - 2 
- end
--- x, y, w, h all represent the player's rectangle. The other values are a rectangle in the upper corner (BOTTOM WALL)
-if cc(x, y, w, h,   0, 546, 801, 64) then  
-  -- if true, decrease HP:
-  y = y - 2
+if cc(x, y, w, h,   230, 227, 25, 32) then  
+  if love.keyboard.isDown('a') and y > 64 then   -- if the 'w' key is being pressed
+    x = x + 2
+  end
  end
 end
 
@@ -75,4 +72,7 @@ function love.draw()
   love.graphics.draw(playerImg, x, y)
 
   love.graphics.print(hp, 0, 0)
+  
+  -- Draw the rectangle in the upper left corner
+  love.graphics.rectangle('line', 195, 195, 64, 64)
 end
