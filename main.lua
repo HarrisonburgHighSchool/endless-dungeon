17ccb5cf79024c24641f0fb91c552746b8e4a99c
Travis <dtrode59@harrisonburg.k12.va.us>
2019-10-09 09:44:50 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ function love.load()
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
   cage = love.graphics.newImage('assets-1/dungeon/floor/cage_0.png')
   mapTemplate = {
-    {cage, cage,  cage, cage,cage,cage,cage,cage,cage,cage,cage,cage,cage,cage,cage,},     
+    {cage, cage, cage, cage, cage,scage,cage,cage,cage,cage,cage,cage,cage,cage,cage,},     
     {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
     {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
     {cage, cobalt, cobalt, cobalt,cobalt,cobalt,cobalt,cobalt,cobalt,cage,cage,cage,},
@@ -27,25 +27,23 @@ function love.load()
   map = Map:new(mapTemplate)
 end
 
-
-
 function love.update(dt)
   if love.keyboard.isDown('right') then
     x = x + 6
   end
-    if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
+    if love.keyboard.isDown('up') then   
       y = y - 6
     end
-    if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
+    if love.keyboard.isDown('down') then   
       y = y  + 6
     end
-    if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
+    if love.keyboard.isDown('left') then   
       x = x - 6
     end
   end
 
 function love.draw()
   map:draw()
-  love.graphics.print('Hello, world!', 0, 0)
+  love.graphics.print('hello', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
