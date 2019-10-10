e9a584e0b3a0b6d652174b4131bbd8c41378787d
Logan <ldlope30@harriosnburg.k12.va.us>
2019-10-10 13:30:02 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,35 +5,55 @@ love.graphics.setDefaultFilter( 'nearest', 'nearest')
 function love.load()
   x = 400
   y = 300
+  x2 = 400
+  y2 = 300
   x1 = 200
   y1 = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
   w = 64   -- The player's width is 64
   h = 64   -- The player's height is 64
   hp = 100 -- Set the player's HP to 100 at the start of the game
-  cam = gamera.new(-500, -500, 2000, 2000)
+  cam = gamera.new(-100, -100, 3000, 2400)
   playerImg = love.graphics.newImage('assets-1/player/base/lorc_male_6.png')
 floorTile = love.graphics.newImage('assets-1/dungeon/floor/grass/grass_2.png')
 wall= love.graphics.newImage('assets-1/dungeon/floor/dirt_0.png')
 template = { --a 3 x 3 map with the altar texture in the middle
              {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
              {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall, wall, wall, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall, wall, wall, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
-              {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
+             {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
              {wall, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wall},
              {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
            }
@@ -42,25 +62,55 @@ map = Map:new(template)
 map:changeScale(1)
 end
 function love.update(dt)
-  cam:setPosition(x, y)
+  cam:setPosition(x2, y2)
+  if x < 0 then
+    x = 0
+  end
+  if y < 0 then
+    y = 0
+  end
+  if y > 950 then
+    y = 950
+  end
+  if x > 2400 then
+    x = 2400
+  end
 if love.keyboard.isDown('right') then
-x = x + 2
+x = x + 5
 end
 if love.keyboard.isDown('left') then
-x = x - 2
+x = x - 5
 end
 if love.keyboard.isDown('down') then
-y = y + 2
+y = y + 5
 end
 if love.keyboard.isDown('up') then
-y = y - 2
+y = y - 5
+end
+if love.keyboard.isDown('right') then
+x2 = x2 + 5
+end
+if love.keyboard.isDown('left') then
+x2 = x2 - 5
+end
+if love.keyboard.isDown('down') then
+y2 = y2 + 5
+end
+if love.keyboard.isDown('up') then
+y2 = y2 - 5
 end
 if cc(x, y, w, h,   0, 0, 64, 64) then
   -- if true, decrease HP:
   hp = hp - 1
 end
---if(hp==0)then
-
+if hp==0 then
+x = 1000000000
+y = 1000000000
+end
+if hp==0 then
+x2 = 1500
+y2 = 1200
+end
 end
 
 function love.draw()
