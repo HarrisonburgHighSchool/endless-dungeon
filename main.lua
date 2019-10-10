a3a5fb1ca9740c03f6bfee9fa0f95b93bbdb1c87
Jon Stapleton <jstapleton@harrisonburg.k12.va.us>
2019-10-10 14:31:59 -0400

    collision in map

--- main.lua
+++ main.lua
@@ -6,23 +6,52 @@ function love.load()
   y = 300
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
   template = {
-    {playerImg, playerImg, playerImg},
-    {playerImg, playerImg, playerImg},
-    {playerImg, playerImg, playerImg},
+    {playerImg, playerImg, nil},
+    {playerImg, nil, nil},
+    {playerImg, nil, nil},
   }
 
   map = Map:new(template)
-
+  debug = false
 end
+
+
+
 
 function love.update(dt)
+
+  mapc = map:cc(x, y, 64, 64)
+  debug = tostring(mapc)
   if love.keyboard.isDown('right') then
-    x = x + 1
+    if map:cc(x + 1, y, 64, 64) == false then
+      x = x + 1
+    end
   end
+  if love.keyboard.isDown('left') then
+    if map:cc(x - 1, y, 64, 64) == false then
+      x = x - 1
+    end
+  end
+  if love.keyboard.isDown('up') then
+    if map:cc(x, y-1, 64, 64) == false then
+      y = y - 1
+    end
+  end
+  if love.keyboard.isDown('down') then
+    if map:cc(x, y+1, 64, 64) == false then
+      y = y + 1
+    end
+  end
+
+
 end
+
+
+
+
 
 function love.draw()
   map:draw()
-  love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
+  love.graphics.print(tostring(mapc), 0, 0)
 end
