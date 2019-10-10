a9af4f45191797036d7850d56296a67fec577d58
Daniel V <davarg@harrisonburg.k12.va.us>
2019-10-10 09:55:22 -0400

    Beta V0.0.8

--- main.lua
+++ main.lua
@@ -19,9 +19,11 @@ function love.load()
   vgate1 = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_up.png')
   vgate2 = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_down.png')
   wall = love.graphics.newImage('assets-1/dungeon/wall/lab-metal_0.png') 
+  entryprtl = love.graphics.newImage('a/prtl.png')
+  entryprtl2 = love.graphics.newImage('a/prtl2.png')
   template = {
     {wall, wall, wall,vgate1, vgate, vgate2, wall, wall, wall, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, entryprtl,  entryprtl2, cobalt, cobalt,cobalt, cobalt, wall},
     {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
     {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
     {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
@@ -59,21 +61,19 @@ function love.update(dt)
   if love.keyboard.isDown('s') then   -- if the 'up' key is being pressed...
     y = y + 4
   end
-
   if love.keyboard.isDown('d') then   -- if the 'up' key is being pressed...
     x = x + 4
   end
-if not cc(x, y, w, h,   0, 0, 1280, 63) then  
- 
+if not cc(x, y, w, h, 0, 0, 1280, 63) then  
   if love.keyboard.isDown('w') then   -- if the 'up' key is being pressed...
     y = y - 4
   end
-  if not cc(x, y, w, h,   0, 0, 0, 200) then
+end
+  if not cc(x, y, w, h, 0, 0, 63, 200) then
     if love.keyboard.isDown('a') then   -- if the 'up' key is being pressed...
       x = x - 4
   end
  end
-end
 cam:setPosition(x,y)
 end
 
