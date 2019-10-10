4bf24b639a5867d20879111c88b6dc5db37e6296
Brian <besont40@harrisonburg.k12.va.us>
2019-10-10 09:48:39 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,8 +10,8 @@ function love.load()
   x = 400
   y = 300
   playerImg = love.graphics.newImage('assets-1/monster/demons/hellion.png')
-  w = 64   -- The player's width is 64
-  h = 899   -- The player's height is 64
+ -- w = 64   -- The player's width is 64
+ -- h = 899   -- The player's height is 64
   w2 = 64   -- The player's width is 64
   h2 = 899
   hp = 100 -- Set the player's HP to 100 at the start of the game
@@ -63,19 +63,18 @@ function love.update(dt)
   end
   cam:setPosition(x,y)
 
-  if not cc(x, y, w, h,  0, 0, 64, 899) then  
+ -- if not cc(x, y, w, h,  0, 0, 64, 899) then  
     -- if true, decrease HP:
-    if love.keyboard.isDown('left') then
-      x = x - 3
-    end
+  --  if love.keyboard.isDown('left') then
+  --    x = x - 3
+   -- end
 end
-
+if love.keyboard.isDown('up') then 
   if cc(x,y,64,64,100,200,64,64)==false then
-    if love.keyboard.isDown('w') then 
       y=y-1
   end
 end
-end
+--end
 
 function love.draw()
  -- player:draw()
@@ -83,7 +82,7 @@ function love.draw()
     map:draw()
     love.graphics.print('', 0, 0)
     love.graphics.draw(playerImg, x, y)
-    love.graphics.rectangle('line', 0, 0, 64, 899)
+  --  love.graphics.rectangle('line', 0, 0, 64, 899)
     love.graphics.print(hp, 0, 0)
 end)
 end
