e84747dccb4295243d90c2188c37fbf11e59415e
Jacob <joyear62@harrisonburg.k12.va.us>
2019-10-11 09:49:44 -0400

    dungeon game

--- main.lua
+++ main.lua
@@ -21,66 +21,85 @@ function love.load()
   doorway = love.graphics.newImage('assets-2/dc-dngn/gateways/dngn_enter_dis.png')
   
 
-  template = { --a 3 x 3 map with the altar texture in the middle
+  template = {
  
   {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall, wall},
-  {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
+  {doorway, rect1Floor, rect2Floor, rect3Floor, rectFloor, rect1Floor, rect2Floor, rect3Floor, wall},
   {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
 }
+
+
+ -- Create the collision map, with walls around the edge of the map
+--   collision = {
+--     {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+--   {nil, nil, nil, nil, nil, nil, nil, nil, wall},
+--   {nil, nil, nil, nil, nil, nil, nil, nil, wall},
+--   {nil, nil, nil, rect3Floor, nil, nil, nil, nil, wall},
+--   {nil, nil, nil, nil, nil, nil, nil, nil, wall},
+--   {nil, nil, nil, nil, nil, nil, nil, nil, wall},
+--   {nil, nil, nil, nil, nil, nil, nil, nil, wall},
+--   {nil, nil, nil, nil, nil, nil, nil, nil, wall},
+--   {nil, nil, nil, nil, nil, nil, nil, nil, wall},
+--   {nil, nil, nil, nil, nil, nil, nil, nil, wall},
+--   {nil, nil, nil, nil, nil, nil, nil, nil, wall},
+--   {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+
+-- }
+
   map = Map:new(template)
-  end
+  -- cmap = Map:new(collision)
+end
 function love.update(dt)
  
--- x, y, w, h all represent the player's rectangle. The other values are blocks you're colliding with.
--- If the statement is true it will run the code, but if it is false it will skip it.
+--x, y, w, h all represent the player's rectangle. The other values are blocks you're colliding with.
+--If the statement is true it will run the code, but if it is false it will skip it.
   
--- if love.keyboard.isDown('w') and y > 18 then -- up
---   if cc(x, y - 2, 64, 64,   230, 227, 25, 32) == false then
+-- if love.keyboard.isDown('w') then -- up
+--   if collision:cc(x, y, 64, 64) then
 --     y = y - 2 -- speed
 --   end
 -- end 
--- if love.keyboard.isDown('a') and x > 62 then -- left
---   if cc(x - 2, y, 64, 64,   230, 227, 25, 32) == false then  
+-- if love.keyboard.isDown('a') then -- left
+--   if collision:cc(x, y, 64, 64) then  
 --     x = x - 2 -- speed
 --   end
 -- end
--- if love.keyboard.isDown('s') and y < 482 then -- down
---   if cc(x, y + 2, 64, 64,   230, 227, 25, 32) == false then  
+-- if love.keyboard.isDown('s') then -- down
+--   if collision:cc(x, y, 64, 64) then  
 --     y = y + 2 -- speed
 --   end
 -- end
--- if love.keyboard.isDown('d') and x < 677 then -- right
---   if cc(x + 2, y, 64, 64,   230, 227, 25, 32) == false then  
+-- if love.keyboard.isDown('d') then -- right
+--   if collision:cc(x, y, 64, 64) then  
 --     x = x + 2 -- speed
 --   end
---  end
+ 
 
 local dx = 0
 local dy = 0
-if love.keyboard.isDown('w') then
+if love.keyboard.isDown('w') and y > 18 then
   dy = - 2
 end
-if love.keyboard.isDown('a') then
+if love.keyboard.isDown('a') and  x > 61 then
   dx = - 2
 end
-if love.keyboard.isDown('s') then
+if love.keyboard.isDown('s') and y < 482 then
   dy = 2
 end
-if love.keyboard.isDown('d') then
+if love.keyboard.isDown('d') and x < 677 then
   dx = 2
 end
 
-if cc(x + dx, y + dy, 64, 64, 230, 227, 25, 32) == false then
+if cc(x + dx, y + dy, 64, 64, 230, 227, 21, 30) == false then
   x = x + dx
   y = y + dy
 end
@@ -90,6 +109,7 @@ end
 
 function love.draw()
   map:draw()
+  -- cmap:draw()
   love.graphics.draw(playerImg, x, y)
 
   love.graphics.print(hp, 0, 0)
