da804c0735f78bea3fb4097008534df2ba43e530
Brian <besont40@harrisonburg.k12.va.us>
2019-10-11 09:53:26 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,13 +7,22 @@ function love.load()
  -- -img = love.graphics.newImage(,player.png)
   --player = Entity:new(img, 200, 200)
   cam = gamera.new(0,0,2000,2000)
-  x = 400
-  y = 300
+  x = 100
+  y = 100
+  x2 = 400
+  y2 = 333
+  x3 = 0
+  y3 = 0
+
+  
+  
   playerImg = love.graphics.newImage('assets-1/monster/demons/hellion.png')
- -- w = 64   -- The player's width is 64
- -- h = 899   -- The player's height is 64
-  w2 = 64   -- The player's width is 64
-  h2 = 899
+ w = 64   -- The player's width is 64
+ h = 64   -- The player's height is 64
+  w2 = 320   -- The player's width is 64
+  h2 = 256
+  w3 = 1088
+  h3 = 64
   hp = 100 -- Set the player's HP to 100 at the start of the game
   floorTile = love.graphics.newImage('assets-1/dungeon/floor/etched_5.png')
   floorTile2 = love.graphics.newImage('assets-1/dungeon/wall/pebble_red_0.png')
@@ -51,19 +60,29 @@ end
 function love.update(dt)
   -- Nothing update
  -- Set up player movement
- if love.keyboard.isDown('up') then
+ if cc(x, y - 3, w, h, x3, y3, w3, h3) == false then
+if cc(x, y - 3, w, h, x2, y2, w2, h2) == false then
+ if love.keyboard.isDown('w') then
   y = y - 3
   end
-  if love.keyboard.isDown('down') then
+end
+end
+if cc(x, y + 3, w, h, x2, y2, w2, h2) == false then
+  if love.keyboard.isDown('s') then
     y = y + 3
   end
-  
-  if love.keyboard.isDown('right') then
-    x = x + 3
+end
+
+if cc(x - 3, y, w, h, x2, y2, w2, h2) == false then 
+  if love.keyboard.isDown('a') then
+    x = x - 3
   end  
- if love.keyboard.isDown('right') then
+end
+if cc(x + 3, y, w, h, x2, y2, w2, h2) == false then 
+ if love.keyboard.isDown('d') then
   x = x + 3
   end
+end
   cam:setPosition(x,y)
 
  -- if not cc(x, y, w, h,  0, 0, 64, 899) then  
@@ -72,9 +91,11 @@ function love.update(dt)
   --    x = x - 3
    -- end
 end
+  
 if love.keyboard.isDown('up') then 
   if cc(x,y,64,64,100,200,64,64)==false then
-      y=y-1
+      y = y - 1
+  end
 
   mapc = map:cc(x, y, 64, 64)
   debug = tostring(mapc)
@@ -95,13 +116,15 @@ if love.keyboard.isDown('up') then
   end
   if love.keyboard.isDown('down') then
     if map:cc(x, y+1, 64, 64) == false then
+  if love.keyboard.isDown('down') then
+    if map:cc(x, y+1, 64, 64) == false then
       y = y + 1
     end
   end
-
+end
 
 end
---end
+end
 
 
 
@@ -111,9 +134,14 @@ function love.draw()
  -- player:draw()
   cam:draw(function(l, t, w, h)
     map:draw()
+    love.graphics.print(x, 50, 50)
+    love.graphics.print(y, 60, 60)
     love.graphics.print('', 0, 0)
     love.graphics.draw(playerImg, x, y)
+    if map:cc(x, y+1, 64, 64) == false then
+    end
   --  love.graphics.rectangle('line', 0, 0, 64, 899)
     love.graphics.print(hp, 0, 0)
 end)
+
 end
