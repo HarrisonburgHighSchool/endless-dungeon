44f6f3a4908334a96354fcee364b0259395dd3f0
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-10-11 09:39:43 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ function love.load()
   hp = 20
   cam = gamera.new(48, 52, 800, 600)
 
-  playerImg = love.graphics.newImage('assets-1/player/base/Octopode_3.png')
+  playerImg = love.graphics.newImage('assets-1/player/base/octopode_3.png')
   w = 64   -- The player's width is 64
   h = 64   -- The player's height is 64
   mirrorPlayerImg = love.graphics.newImage('assets-1/player/base/Octopode_2.png')
