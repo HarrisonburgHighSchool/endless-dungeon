d157e23220f1dd261f14ec86da91485f0e59459b
Edwin <exgali19@harrisonburg.k12.va.us>
2019-10-14 14:20:13 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,28 @@
 local Map = require 'core/map'
+local Util = require 'core/util'
 
 function love.load()
   x = 370
   y = 270
 playerImg = love.graphics.newImage('assets-1/player/base/lorc_male_5.png')
+
   floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_9.png')
+  background = {
+    {floor, floor, floor, floor},
+    {floor, floor, floor, floor},
+    {floor, floor, floor, floor},
+    {floor, floor, floor, floor},
+  }
   wallTile = love.graphics.newImage('assets-1/dungeon/wall/crystal_wall_lightblue.png')
+  collision = {
+    {wall, wall, wall, wall},
+    {wall, 'nil', 'nil', wall},
+    {wall, 'nil', 'nil', wall},
+    {wall, 'nil', 'nil', wall},
+  }
+  collision = Map:new(collision)
+  background = Map:new(background)
+
   --wall     = love.graphics.newImage('altar-asset.png')
   template = { --a 3 x 3 map with the altar texture in the middle
                {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
@@ -13,9 +30,9 @@ playerImg = love.graphics.newImage('assets-1/player/base/lorc_male_5.png')
                {floorTile, floorTile, wallTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
                {floorTile, floorTile, wallTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
                {floorTile, floorTile, wallTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
-               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
-               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
-               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, wallTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wallTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, wallTile, floorTile, floorTile},
                {floorTile, floorTile, floorTile, wallTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
                {floorTile, floorTile, floorTile, wallTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
                {floorTile, floorTile, floorTile, wallTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
@@ -29,7 +46,7 @@ end
 function love.update(dt)
 
 if love.keyboard.isDown('right') then
-  if collision:cc(x + 1, y, 64, 64) == false then
+  if core/map.cc(x + 1, y, 370, 270) == false then
     x = x + 1
   end
 end
@@ -49,6 +66,8 @@ end
 
 function love.draw()
   map:draw()
+  background:draw()
+  collision:draw()
   love.graphics.print('Yo!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
