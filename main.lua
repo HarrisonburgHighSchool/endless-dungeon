e4d93b621fd6b4d96c5b69e7148a9be18c897a16
Jason Castro <jxcast49@harrisonburg.k12.va.us>
2019-10-14 14:28:55 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,6 +13,7 @@ function love.load()
   tile3 = love.graphics.newImage('assets-1/dungeon/wall/bars_red_1.png')
   playerImg = love.graphics.newImage('assets-1/player/base/naga_red_female.png')
 
+wall = love.graphics.newImage('assets-1/dungeon/wall/bars_red_2.png')
   map = {
     {tile, tile, tile, tile2, tile2, tile2, tile, tile, tile, tile},
     {tile, tile, tile ,tile2, tile2, tile2, tile, tile, tile, tile},
@@ -27,8 +28,16 @@ function love.load()
     {tile, tile, tile, tile2, tile2, tile, tile, tile, tile, tile},
     {tile, tile, tile, tile2, tile2, tile2, tile, tile, tile, tile},
     {tile, tile, tile, tile2, tile3, tile3, tile, tile, tile, tile}
+}
+collision = {
+    {wall, wall, wall, wall},
+    {wall, 'nil', 'nil', wall},
+    {wall, 'nil', 'nil', wall},
+    {wall, 'nil', 'nil', wall},
   }
+
   bkgrnd = Map:new(map)
+  collision = Map:new(collision)
 
 end
 
@@ -36,12 +45,12 @@ end
 
 function love.update(dt)
   if love.keyboard.isDown('up') then
-    if cc(x, y - 10, 64, 64, 100, 200, 64, 64) == false then
+    if collision:cc(x, y - 10, 64, 64) == false then
       y = y - 10
     end
   end
   if love.keyboard.isDown('down') then
-    if cc(x, y + 10 , 64, 64, 100, 200, 64, 64)  == false then
+    if collision:cc(x, y + 10 , 64, 64)  == false then
       y = y + 10
     end
   end
@@ -62,6 +71,7 @@ end
 
 function love.draw()
   bkgrnd:draw()
+  collision:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
