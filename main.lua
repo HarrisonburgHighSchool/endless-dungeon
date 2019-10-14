4fe317ab253d3274b39589be7ebe8114410758d4
Corey <cdrupe35@harrisonburg.k12.va.us>
2019-10-14 09:51:12 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,12 +4,16 @@ enter = 1
 collide = false
   x = 385
   y = 60
+  w = 38
+  h = 60
   x2 = 191
   y2 = 192
-  w2 = 64
-  h2 = 64
-  w = 64
-  h = 64
+  w2 = 128
+  h2 = 128
+  x3 = 447
+  y3 = 62
+  w3 = 192
+  h3 = 192
   hp = 100
   playerImg = love.graphics.newImage('assets-1/player/base/gargoyle_male.png')
 
@@ -26,17 +30,17 @@ collide = false
 
   template = { 
                {wall, wall1, wall3, wall1, wall3, wall2, wall3, wall, wall},
-               {wall1, floor1, floor1, floor, floor, floor, floor1, floor1, wall},
-               {wall, floor1, floor, floor, floor, floor, floor, floor1, wall},
-               {wall3, floor, floor, lava, floor, floor, floor1, floor, wall1},
-               {wall1, floor, floor, floor, floor, floor, floor, floor, wall},
-               {banner, floor, floor, floor, floor, floor, floor, floor, wall3},
-               {wall_enter, floor1, floor, floor, floor, floor, floor, floor, wall1},
-               {banner, floor, floor, floor, floor, floor, floor1, floor, wall},
-               {wall, floor, floor, floor, floor1, floor, floor, floor, wall1},
-               {wall1, floor, floor, floor, floor, floor, floor, floor, wall2},
-               {wall3, floor1, floor, floor, floor, floor, floor, floor1, wall1},
-               {wall, floor1, floor1, floor, floor, floor, floor1, floor, wall3},
+               {wall1, floor1, floor1, floor, lava, floor, floor1, floor1, wall},
+               {wall, floor1, lava, floor, floor, floor, lava, floor1, wall},
+               {wall3, floor, lava, lava, lava, lava, floor1, floor, wall1},
+               {wall1, floor, floor, lava, lava, floor, floor, lava, wall},
+               {banner, lava, floor, lava, floor, floor, lava, lava, wall3},
+               {wall_enter, floor1, floor, lava, floor, lava, lava, lava, wall1},
+               {banner, lava, lava, lava, floor, lava, lava, lava, wall},
+               {wall, lava, lava, lava, floor1, floor, floor, floor, wall1},
+               {wall1, lava, lava, lava, lava, lava, lava, floor, wall2},
+               {wall3, floor1, floor, floor, lava, lava, lava, floor1, wall1},
+               {wall, floor1, lava, floor, floor, floor, floor1, floor, wall3},
                {wall3, wall1, wall1, wall3, wall1, wall, wall, wall, wall},
              }
     map = Map:new(template)
@@ -46,18 +50,18 @@ end
 
 
 function love.update(dt)
-if love.keyboard.isDown('right') then 
-  x = x + 2
-end
+  if love.keyboard.isDown('right') then 
+    x = x + 2
+  end
   if love.keyboard.isDown('left') then 
     x = x - 2
-end
-if love.keyboard.isDown('up') then 
-  y = y - 2
-end
+  end
+  if love.keyboard.isDown('up') then 
+    y = y - 2
+  end
   if love.keyboard.isDown('down') then 
-    y = y + 2
-end
+     y = y + 2
+  end
 if(x < 51)then 
     x = x + 2
 end
@@ -71,6 +75,11 @@ if(y > 450)then
   y = y - 2
 end
 if(x + w > x2 and y + h > y2 and y2 + h2 > y and x2 + w2 > x) then 
+  collide = true
+else
+  collide = false
+end
+if(x + w > x3 and y + h > y3 and y3 + h3 > y and x3 + w3 > x) then 
   collide = true
 else
   collide = false
