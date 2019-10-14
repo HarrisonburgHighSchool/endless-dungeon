e54e2247abbdb30640a7fa52718f68add468a2b8
Vyshon <vowash62@harisonburg.k12.va.us>
2019-10-14 14:01:26 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,9 +8,30 @@ function love.load()
   floorTile = love.graphics.newImage('assets-1/dungeon/floor/bog_green_3.png')
    altar     = love.graphics.newImage('assets-1/dungeon/floor/bog_green_3.png')
    template = { --a 3 x 3 map with the altar texture in the middle
-                {floorTile, floorTile, floorTile},
-                {floorTile, altar, floorTile},
-                {floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, altar, floorTile ,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, altar, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, altar, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, altar, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, altar, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, altar, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, altar, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, altar, floorTile,floorTile, floorTile, floorTile},
+                {floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
               }
    map = Map:new(template)
 end
@@ -33,4 +54,12 @@ function love.draw()
   map:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
+end
+wall = love.graphics.newImage('assets-1/dungeon/wall/catacombs_1.png')
+  collision = {
+    {wall, wall, wall, wall},
+    {wall, 'nil', 'nil', wall},
+    {wall, 'nil', 'nil', wall},
+    {wall, 'nil', 'nil', wall},
+
 end
