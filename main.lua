2d7e5f35ddab885dcef5607667a37364b3451c88
david <dtkoml19@harrisonburg.k12.va.us>
2019-10-14 14:34:16 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,104 +3,109 @@ local Entity = require 'core/entity'
 
 function love.load()
   playerImg = love.graphics.newImage('assets-1/player/base/naga_red_female.png')
-  player = Entity:new(playerImg, 200, 200)
-end
+  x = 200
+  y = 0
 
-function love.update(dt)
-end
-
-function love.draw()
-  player:draw()
 end
 
 
-
-
-
-local Util = require 'core/util'
-function love.load()
-  x = 400
-  y = 300
-  w1 = 5
-  h1 = 64
-  hp = 100
-end
-local Map = require 'core/map'
-
-
-
-  x1 = 10
-  y1 = 64
-  x2 = 50
-  y2 = 50
-  w2 = 64
-  h2 = 64
 
   tile = love.graphics.newImage('assets-1/dungeon/floor/etched_1.png')
-  tile2 = love.graphics.newImage('assets-1/dungeon/wall/crystal_wall_blue.png')
-  tile3 = love.graphics.newImage('assets-1/dungeon/wall/bars_red_1.png')
+  floor = love.graphics.newImage('assets-1/dungeon/wall/crystal_wall_blue.png')
+  background = {
+
+
+  }
+  wall = love.graphics.newImage('assets-1/dungeon/wall/bars_red_1.png')
   tile4 = love.graphics.newImage('assets-1/dungeon/floor/acidic_floor_0.png')
   tile5 = love.graphics.newImage('assets-1/dungeon/floor/acidic_floor_1.png')
   tile6 = love.graphics.newImage('assets-1/dungeon/floor/acidic_floor_2.png')
   tile7 = love.graphics.newImage('assets-1/dungeon/floor/acidic_floor_3.png')
 
   map = {
-    {tile4, tile5, tile6, tile2, tile2, tile2, tile4, tile5, tile6, tile7},
-    {tile7, tile4, tile5, tile2, tile2, tile2, tile7, tile4, tile5, tile6},
-    {tile6, tile7, tile4, tile2, tile2, tile2, tile6, tile7, tile4, tile5},
-    {tile5, tile6, tile7, tile2, tile2, tile2, tile5, tile6, tile7, tile4},
-    {tile4, tile5, tile6, tile2, tile2, tile2, tile4, tile5, tile6, tile7},
-    {tile2, tile2, tile2, tile2, tile2, tile2, tile2, tile2, tile2, tile3},
-    {tile2, tile2, tile2, tile2, tile2, tile2, tile2, tile2, tile2, tile3},
-    {tile2, tile2, tile2, tile2, tile2, tile2, tile2, tile2, tile2, tile3},
-    {tile5, tile6, tile7, tile2, tile2, tile2, tile4, tile5, tile6, tile7},
-    {tile6, tile7, tile4, tile2, tile2, tile2, tile5, tile6, tile7, tile4},
-    {tile7, tile4, tile5, tile2, tile2, tile2, tile6, tile7, tile4, tile5},
-    {tile4, tile5, tile6, tile2, tile2, tile2, tile7, tile4, tile5, tile6},
-    {tile5, tile6, tile7, tile3, tile3, tile3, tile4, tile5, tile6, tile7}
+    {tile4, tile5, tile6, floor, floor, floor, tile4, tile5, tile6, tile7},
+    {tile7, tile4, tile5, floor, floor, floor, tile7, tile4, tile5, tile6},
+    {tile6, tile7, tile4, floor, floor, floor, tile6, tile7, tile4, tile5},
+    {tile5, tile6, tile7, floor, floor, floor, tile5, tile6, tile7, tile4},
+    {tile4, tile5, tile6, floor, floor, floor, tile4, tile5, tile6, tile7},
+    {floor, floor, floor, floor, floor, floor, floor, floor, wall, wall},
+    {floor, floor, floor, floor, floor, floor, floor, floor, wall, 'nil'},
+    {floor, floor, floor, floor, floor, floor, floor, floor, wall, wall},
+    {tile5, tile6, tile7, floor, floor, floor, tile4, tile5, tile6, tile7},
+    {tile6, tile7, tile4, floor, floor, floor, tile5, tile6, tile7, tile4},
+    {tile7, tile4, tile5, floor, floor, floor, tile6, tile7, tile4, tile5},
+    {tile4, tile5, tile6, wall, wall, wall, tile7, tile4, tile5, tile6},
+    {tile5, tile6, tile7, wall, 'nil', wall, tile4, tile5, tile6, tile7}
   }
-  bkgrnd = Map:new(map)
 
-
+  collision = Map:new(collision)
+  bkgrnd = Map:new(background)
   map = Map:new(template)
   debug = false
 
 
+  map = map:cc(x, y, 64, 64)
+  debug = tostring(mapc)
+  function love.update(dt)
+    if love.keyboard.isDown('right') then
+      if map:cc(x + 5, y, 64, 64, 100, 200, 64, 64) then
+        x = x + 5
+      end
+    end
+    if love.keyboard.isDown('left') then
+      if map:cc(x - 5, y, 64, 64, 100, 200, 64, 64) then
+        x = x - 5
+      end
+    end
+    if love.keyboard.isDown('up') then
+      if map:cc(x, y-5, 64, 64, 100, 200, 64, 64) then
+        y = y - 5
+      end
+    end
+  if love.keyboard.isDown('down') then
+    if map:cc(x, y+5, 64, 64, 100, 200, 64, 64) then
+      y = y + 5
+    end
+  end
+end
+
+
+  if love.keyboard.isDown('down') then
+    if map:cc(x, y+5, 64, 64, 100, 200, 64, 64) then
+      y = y + 5
+    end
+  end
 
 
 
 function love.update(dt)
-end
-
-function love.draw()
-  map:draw()
-end
-
   if love.keyboard.isDown('right') then
-    if cc(x + 1, y, w1, h1, x2, y2, w2, h2) == false then
-       x = x + 1
+    if collision:cc(x + 5, y, 64, 64) == false then
+       x = x + 5
     end
   end
   if love.keyboard.isDown('left') then
-    if cc(x - 1, y, w1, h1, x2, y2, w2, h2) == false then
-       x = x - 1
+    if collision:cc(x - 5, y, 64, 64) == false then
+       x = x - 5
     end
   end
   if love.keyboard.isDown('up') then
-    if cc(x , y - 1, w1, h1, x2, y2, w2, h2) == false then
-       y = 7 - 1
+    if collision:cc(x, y - 5, 64, 64) == false then
+       y = y - 5
     end
   end
   if love.keyboard.isDown('down') then
-    if cc(x , y + 1, w1, h1, x2, y2, w2, h2) == false then
-       y = y + 1
+    if collision:cc(x , y + 5, 64, 64) == false then
+       y = y + 5
     end
   end
-  if cc(x, y, w, h, 0, 0, 64, 64) then
-    hp = hp - 1
-  end
+end
 
-
+function love.draw()
+  background:draw()
+  collision:draw()
+  love.graphics.draw(img, x, y)
+end
 
 
 function love.draw()
@@ -110,28 +115,7 @@ function love.draw()
 end
 
 
-mapc = map:cc(x, y, 64, 64)
-debug = tostring(mapc)
-if love.keyboard.isDown('right') then
-  if map:cc(x + 1, y, 64, 64) == false then
-    x = x + 1
-  end
-end
-if love.keyboard.isDown('left') then
-  if map:cc(x - 1, y, 64, 64) == false then
-    x = x - 1
-  end
-end
-if love.keyboard.isDown('up') then
-  if map:cc(x, y-1, 64, 64) == false then
-    y = y - 1
-  end
-end
-if love.keyboard.isDown('down') then
-  if map:cc(x, y+1, 64, 64) == false then
-    y = y + 1
-  end
-end
+
 
 
 local gamera = require 'core/gamera'
@@ -147,13 +131,8 @@ end
 
 
 function love.draw()
-  map:draw()
-  love.graphics.draw(playerImg, x, y)
-  love.graphics.print(tostring(mapc), 0, 0)
->>>>>>> upstream/master
-
-
-
   bkgrnd:draw()
-  map:draw()
+  love.graphics.print(tostring(mapc), 0, 0)
   love.graphics.print('Hello, world!', 0, 0)
+end
+end
