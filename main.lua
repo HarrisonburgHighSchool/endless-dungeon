a26a352f9806e62aa7cf4c27b7e9e4eab72b25e7
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-10-14 09:52:54 -0400

    xcvhk

--- main.lua
+++ main.lua
@@ -24,49 +24,72 @@ function love.load()
   
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/limestone_6.png')
   cobaltM = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_flip.png')
-  cobaltMirrorLeft = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_left.png')
-  cobaltMirrorLeftT = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_left_top.png')
-  cobaltMirrorRight = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_right.png')
-  cobaltMirrorRightT = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_right_top.png')
+  MirrorL = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_left.png')
+  MirrorR = love.graphics.newImage('assets-1/dungeon/floor/limestone_6_mirror_right.png')
   hole = love.graphics.newImage('assets-1/dungeon/floor/hole.png')
   wall = love.graphics.newImage('assets-1/dungeon/wall/catacombs_0.png')
   --Hp = love.graphics.newImage('assets-1/player/hp_bar/full.png')
 
-  mapTemplate = {
-    {nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil,},
-    {nil, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, hole, hole, nil,},
-    {nil, cobalt, cobalt, hole, cobalt, cobalt, cobalt, cobalt, hole, hole, nil,},
-    {nil, cobalt, cobalt, hole, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, nil,},
-    {nil, cobalt, cobalt, cobalt, cobalt, cobalt, hole, cobalt, cobalt, cobalt, nil,},
-    {nil, cobalt, hole, hole, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, nil,},
-    {nil, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, nil,},
-    {nil, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, nil,},
-    {nil, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, nil,},
-    {nil, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, nil,},
-    {nil, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, nil,},
-    {nil, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, nil,}, 
-    {nil, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, nil,},
-    {nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil,},
+  floor = {
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, nil, nil,},
+    {cobalt, cobalt, cobalt, nil, cobalt, cobalt, cobalt, cobalt, nil, nil,},
+    {cobalt, cobalt, cobalt, nil, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, nil, cobalt, cobalt, cobalt,},
+    {cobalt, cobalt, nil, nil, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
+    {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,},
+    {cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM,},
+    {cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM,},
+    {cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM,},
+    {cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM,},
+    {cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM,}, 
+    {cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM,},
+    {cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM, cobaltM,},
   }
-  map = Map:new(mapTemplate)
+  wall = {
+    {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall,},
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,},
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,},
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,},
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,},
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,},
+    {wall, MirrorR, MirrorR, MirrorR, MirrorR, MirrorR, MirrorR, MirrorR, MirrorR, MirrorR, wall,},
+    {wall, MirrorL, MirrorL, MirrorL, MirrorL, MirrorL, MirrorL, MirrorL, MirrorL, MirrorL, wall,},
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,},
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,},
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,},
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,}, 
+    {wall, nil, nil, nil, nil, nil, nil, nil, nil, nil, wall,},
+    {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall,},
+  }
+  floor = Map:new(floor)
+  wall = Map:new(wall)
 end
   
 function love.update(dt)
-  if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
-    x = x + 3
-    mirrorx = mirrorx - 3
+  if wall:cc(x + 3, y, 64, 64) == false then
+    if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
+      x = x + 3
+      mirrorx = mirrorx - 3
+    end
   end
-  if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
-    y = y + 3
-    mirrory = mirrory + 3
+  if wall:cc(x, y + 3, 64, 64) == false then
+    if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
+      y = y + 3
+      mirrory = mirrory + 3
+    end
   end
-  if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
-    x = x - 3
-    mirrorx = mirrorx + 3
+  if wall:cc(x - 3, y, 64, 64) == false then
+    if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
+      x = x - 3
+      mirrorx = mirrorx + 3
+    end
   end
-  if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
-    y = y - 3
-    mirrory = mirrory - 3
+  if wall:cc(x, y - 3, 64, 64) == false then
+    if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
+      y = y - 3
+      mirrory = mirrory - 3
+    end
   end
   if cc(x, y, w, h, 216, 216, 84, 16) then  
     hp = hp - 1
@@ -80,36 +103,13 @@ function love.update(dt)
   if cc(x, y, w, h, 412, 152, 12, 72) then  
     hp = hp - 1
   end
-  if cc(x, y, w, h, 64, 0, 768, 64) then  
-    if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
-      y = y + 3
-      mirrory = mirrory + 3
-    end
-  end
-  if cc(x, y, w, h, 502, 64, 84, 576) then  
-    if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
-      x = x - 3
-      mirrorx = mirrorx + 3
-    end
-  end
-  if cc(x, y, w, h, 64, 64, 64, 576) then  
-    if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
-      x = x + 3
-      mirrorx = mirrorx - 3
-    end
-  end
-  if cc(x, y, w, h, 64, 644, 832, 64) then  
-    if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
-      y = y - 3
-      mirrory = mirrory - 3
-    end
-  end
   cam:setPosition(x, y)
 end
 
 function love.draw()
   cam:draw(function(camx, camy)
-  map:draw()
+  floor:draw()
+  wall:draw()
   love.graphics.draw(playerImg, x, y, rotation, -1, 1)
   love.graphics.draw(mirrorPlayerImg, mirrorx, mirrory)
   love.graphics.print(hp, x, y)
