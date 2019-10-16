5f97565c9afa3edec7683a87bf51225ec5072a46
Robel tewolde <rmtewo81@harrisonburg.k12.va.us>
2019-10-16 14:32:47 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -31,64 +31,86 @@ function love.load()
                   {floorTile, altar, altar, altar, altar, altar, altar, altar, altar, floorTile},
                   {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
                 }
+                -- Create the background map
+                floor = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
+                background = {
+                  {floor, floor, floor, floor},
+                  {floor, floor, floor, floor},
+                  {floor, floor, floor, floor},
+                  {floor, floor, floor, floor},
+                }
+                -- Create the collision map, with walls around the edge of the map
+                wall = love.graphics.newImage('assets-1/dungeon/wall/catacombs_1.png')
+                collision = {
+                  {wall, wall, wall, wall},
+                  {wall, 'nil', 'nil', },
+                  {wall, 'nil', 'nil', wall},
+                  {wall, 'nil', 'nil', wall},
+                }
+                  background = Map:new(background)
+                  collision = Map:new(collision)
                   map = Map:new(template)
                   cam = gamera.new(-500, -500, 2000, 2000)
+                  img = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+                  x = 400
+                  y = 300
 
 end
 
 function love.update(dt)
   player2()
   if love.keyboard.isDown('up')then
-       if cc(x, y, 64, 64, 100, 200, 64, 64) == false then
+  if collision:cc(x, y - 10, 64, 64) == false then
    y = y - 10
    cam:setPosition(x, y)
   end
 end
   if love.keyboard.isDown('down')then
-    if cc(x, y, 64, 64, 100, 200, 64, 64) == false then
+  if collision:cc(x, y + 10, 64, 64) == false then
    y = y + 10
    cam:setPosition(x, y)
   end
 end
   if love.keyboard.isDown('right')then
-    if cc(x, y, 64, 64, 100, 200, 64, 64) == false then
+  if collision:cc(x + 10, y, 64, 64) == false then
    x = x + 10
    cam:setPosition(x, y)
   end
 end
   if love.keyboard.isDown('left')then
-    if cc(x, y, 64, 64, 100, 200, 64, 64) == false then
+  if collision:cc(x - 10 , y, 64, 64) == false then
    x = x - 10
    cam:setPosition(x, y)
- end
+  end
 end
    if cc(x, y, w, h,   0, 0, 64, 64) then
       hp = hp - 1
-end
-end
+    end
+  end
+
 
 function player2()
   if love.keyboard.isDown('w')then
+    if collision:cc(a, b - 10, 64, 64) == false then
    b = b - 10
-   if cc(x, y, 64, 64, 100, 200, 64, 64) == false then
    cam:setPosition(a, b)
   end
 end
   if love.keyboard.isDown('s')then
+      if collision:cc(a, b + 10, 64, 64) == false then
    b = b + 10
-   if cc(x, y, 64, 64, 100, 200, 64, 64) == false then
    cam:setPosition(a, b)
   end
 end
   if love.keyboard.isDown('d')then
+    if collision:cc(a + 10 , y, 64, 64) == false then
    a = a + 10
-   if cc(x, y, 64, 64, 100, 200, 64, 64) == false then
    cam:setPosition(a, b)
   end
 end
   if love.keyboard.isDown('a')then
+    if collision:cc(a - 10 , y, 64, 64) == false then
    a = a - 10
-   if cc(x, y, 64, 64, 100, 200, 64, 64) == false then
    cam:setPosition(a, b)
   end
 end
@@ -105,8 +127,10 @@ function love.draw()
     love.graphics.draw(playerImg, x, y)
     love.graphics.rectangle('line', 0, 0, 64, 64)
     love.graphics.print(hp, 0, 0)
-    love.graphics.draw(playerImg, x, y)
+    love.graphics.draw(playerImg1, a, b)
     love.graphics.rectangle('line', 0, 0, 64, 64)
     love.graphics.print(hp, 0, 0)
+    background:draw()
+    collision:draw()
   end)
 end
