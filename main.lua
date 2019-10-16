f3f7b3804a01d0913b31ca7ab3223ed2e6e1c402
Robi <rahail79@iMac714-462.local>
2019-10-16 14:31:40 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ function love.load()
   x = 400
   y = 300
 
-playerImg = love.graphics.newImage('assets-1/player/base/kobold_male.png')
+playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
 wall =love.graphics.newImage('assets-1/dungeon/wall/catacombs_0.png')
   floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_7.png')
   altar     = love.graphics.newImage('assets-1/dungeon/floor/cage_3.png')
@@ -29,16 +29,16 @@ template = {
 
 }
 wall={
-             -- {wall,wall,wall,wall},
-             -- {wall,'nil','nil',wall},
-             -- {wall,'nil','nil',wall},
-             -- {wall,'nil','nil',wall},
-             -- {wall,'nil','nil',wall},
-             -- {wall,'nil','nil',wall},
-             -- {wall,'nil','nil',wall},
-             -- {wall,'nil','nil',wall},
-             {wall, wall, wall, wall},
-             {wall, wall, 'nil', wall},
+             {wall,wall,wall,wall},
+             {wall,'nil','nil',wall},
+             {wall,'nil','nil',wall},
+             {wall,'nil','nil',wall},
+             {wall,'nil','nil',wall},
+             {wall,'nil','nil',wall},
+             {wall,'nil','nil',wall},
+             {wall,'nil','nil',wall},
+             {wall,wall,wall,wall},
+
 
             }
 
@@ -52,34 +52,40 @@ end
 
 
 function love.update(dt)
-if collision:cc(x,y-3,w,h, 0,0,64,64) == false then
+if collision:cc(x,y-5,w,h, 0,0,64,64) == false then
   if love.keyboard.isDown('up')then
-      y=y-3
+      y=y-5
       cam:setPosition(x, y)
   end
  end
-if collision:cc(x,y+3,w,h, 0,0,64,64) == false then
+if collision:cc(x,y+5,w,h, 0,0,64,64) == false then
    if love.keyboard.isDown('down')then
-    y=y+3
+    y=y+5
     cam:setPosition(x, y)
   end
 end
-if collision:cc(x-3,y,w,h, 0,0,64,64) == false then
+if collision:cc(x-5,y,w,h, 0,0,64,64) == false then
   if love.keyboard.isDown('left')then
-    x=x-3
+    x=x-5
     cam:setPosition(x, y)
   end
 end
- if collision:cc(x+3,y,w,h, 0,0,64,64) == false then
+ if collision:cc(x+5,y,w,h, 0,0,64,64) == false then
  if love.keyboard.isDown('right')then
-    x=x+3
+    x=x+5
     cam:setPosition(x, y)
   end
  end
-  if love.keyboard.isDown('right')then
-   x=x+3
-   cam:setPosition(x, y)
-  end
+
+ if x < 150 and x > 50 and y > 250 and y < 250 then
+   y = y + 200
+   x = x + 400
+end
+
+if x < 150 and x > 50 and y > 350 and y < 450 then
+  y = y + 200
+  x = x + 400
+end
 
 end
 
@@ -89,10 +95,14 @@ cam:draw(function(l, t, w, h)
   map:draw()
   collision:draw()
   love.graphics.print('Hello, world!', 0, 0)
+
   love.graphics.draw(playerImg, x, y)
 
+ love.graphics.rectangle('line', 100, 300, 64, 64)
+
+  love.graphics.rectangle('line', 100, 400, 64, 64)
  end)
- love.graphics.rectangle('line', 0, 0, 64, 64)
+
 
 
 love.graphics.print(hp, 0, 0)
