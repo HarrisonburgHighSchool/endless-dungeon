4ebb49b873d0314d124372c382c109a24f501ec4
David Cervantes <dxcerv19@harriosnburg.k12.va.us>
2019-10-17 14:01:04 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,11 +8,14 @@ function love.load()
   b = 100
   x = 400
   y = 300
+  ex = 100
+  ey = 100
   playerImg1 = love.graphics.newImage('assets-1/dungeon/trees/mangrove_1.png')
   w = 64   -- The player's width is 64
   h = 64   -- The player's height is 64
   hp = 100 -- Set the player's HP to 100 at the start of the game
   playerImg2 = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  eimg = love.graphics.newImage('assets-1/player/base/octopode_2.png')
   floorTile = love.graphics.newImage('assets-1/dungeon/floor/bog_green_1.png')
   wall     = love.graphics.newImage('assets-1/dungeon/wall/hell_5.png')
  template = { --a 3 x 3 map with the altar texture in the middle
@@ -70,6 +73,19 @@ function love.update(dt)
   if a > 1250 then
     a = 1250
   end
+  if ex < 0 then
+    ex = 0
+  end
+  if ey < 0 then
+    ey = 0
+  end
+  if ey > 540 then
+    ey = 540
+  end
+  if ex > 1250 then
+    ex = 1250
+  end
+
   cam:setPosition(x, y)
 
   if love.keyboard.isDown('right') then
@@ -96,13 +112,24 @@ function love.update(dt)
             end
           if love.keyboard.isDown('w') then
                 b = b - 8
-
-          end
+              end
+          if love.keyboard.isDown('right') then
+                ex = ex + 8
+              end
+         if love.keyboard.isDown('left') then
+           ex = ex - 8
+         end
+         if love.keyboard.isDown('down') then
+           ey = ey + 8
+         end
+         if love.keyboard.isDown('up') then
+           ey = ey - 8
+         end
+       end
   if cc(x, y, w, h,   0, 0, 64, 64) then
     hp = hp - 1
   end
 
-end
 
 function love.draw()
 
@@ -111,6 +138,8 @@ function love.draw()
       love.graphics.draw(playerImg1, x, y)
       love.graphics.rectangle('line', 0, 0, 64, 64)
       love.graphics.print(hp, 0, 0)
+      -- Draw the enemy
+      love.graphics.draw(eimg, ex, ey)
 
       love.graphics.print('Hello, world!', 0, 0)
       love.graphics.draw(playerImg2, a, b)
