507c412a3a0877130a26b01f91fcb98c77c59b22
elijah reed <emreed18@harrisonburg.k12.va.us>
2019-10-17 14:16:33 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,9 +3,9 @@ local Map = require 'core/map'
 function love.load()
 
 
-  9p0x = 400
+  x = 400
   y = 300
-
+img = love.graphics.newImage('assets-1/player/base/octopode_1.png')
   tile = love.graphics.newImage('assets-1/dungeon/floor/etched_1.png')
   tile2 = love.graphics.newImage('assets-1/dungeon/floor/cage_1.png')
   tile3 = love.graphics.newImage('assets-1/dungeon/doors/vgate_sealed_up.png')
@@ -28,31 +28,55 @@ function love.load()
     {tile4, tile, tile, tile3, tile3, tile3, tile, tile, tile, tile}
   }
   bkgrnd = Map:new(map)
+  floor = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
+    background = {
+      {floor, floor, floor, floor},
+      {floor, floor, floor, floor},
+      {floor, floor, floor, floor},
+      {floor, floor, floor, floor},
+    }
+    wall = love.graphics.newImage('assets-1/dungeon/wall/catacombs_1.png')
+      collision = {
+        {wall, wall, wall, wall},
+        {wall, 'nil', 'nil', wall},
+        {wall, 'nil', 'nil', wall},
+        {wall, 'nil', 'nil', wall},
+      }
+      background = Map:new(background)
+        collision = Map:new(collision)
+
 
 end
-
-
 
 function love.update(dt)
-  if love.keyboard.isDown('right') then
-    x=x+10
-  end
-  if love.keyboard.isDown('left') then
-    x=x-10
-  end
   if love.keyboard.isDown('up') then
-    y=y-10
+    if collision:cc(x, y - 1, 64, 64) == false then
+      y = y - 1
+    end
   end
   if love.keyboard.isDown('down') then
-    y=y+10
+    if collision:cc(x, y + 1, 64, 64) == false then
+      y = y + 1
+    end
+  end
+  if love.keyboard.isDown('right') then
+    if collision:cc(x + 1, y, 64, 64) == false then
+      x = x + 1
+    end
+  end
+  if love.keyboard.isDown('left') then
+    if collision:cc(x - 1, y, 64, 64) == false then
+      x = x - 1
+    end
   end
 end
-
 
 
 
 function love.draw()
   bkgrnd:draw()
+  background:draw()
+  collision:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
