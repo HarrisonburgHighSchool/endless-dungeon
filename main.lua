38c54baf49687046495124db67a26769c271e28e
Daniel V <davarg@harrisonburg.k12.va.us>
2019-10-17 08:47:12 -0400

    Beta V0.1.0

--- main.lua
+++ main.lua
@@ -10,42 +10,38 @@ function love.load()
   y = 300
   --playerImg = love.graphics.newImage('a/images.png')
   playerImg = love.graphics.newImage('assets-1/player/transform/dragon_form_red.png')
-  playerImg2 = love.graphics.newImage('assets-1/player/transform/dragon_form_red.png')
-  x2 = 360
-  y2 = 300
+  x2 = 400
+  y2 = 100
   cam = gamera.new(0, 0, 2000, 2000)
   w = 64
   h = 64
   hp = 100
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/mesh_3.png')
-  vgate = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_middle.png')
-  vgate1 = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_up.png')
-  vgate2 = love.graphics.newImage('assets-1/dungeon/doors/vgate_runed_down.png')
+  prtl = love.graphics.newImage('assets-1/dungeon/gateways/zig_portal90CCW.png')
   wall = love.graphics.newImage('assets-1/dungeon/wall/lab-metal_0.png') 
-  entryprtl = love.graphics.newImage('a/prtl.png')
-  entryprtl2 = love.graphics.newImage('a/prtl2.png')
+  
   template = {
-    {wall, wall, wall,vgate1, vgate, vgate2, wall, wall, wall, wall},
-    {wall, cobalt, cobalt, entryprtl,  entryprtl2, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt,cobalt, cobalt, wall},
-    {wall,wall, wall, wall, wall, wall, wall,wall, wall, wall},
+    {wall, wall, wall, wall, prtl, wall, wall, wall, wall, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, wall},
+    {wall,wall, wall, wall, wall, wall, wall, wall, wall, wall},
   }
 
   
@@ -65,7 +61,7 @@ function love.load()
     {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall},
     {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall},
     {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall},
-    {wall,' nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall},
+    {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall},
     {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall},
     {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall},
     {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall},
@@ -75,6 +71,7 @@ function love.load()
     {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall},
     {wall, wall, wall, wall, wall, wall, wall ,wall, wall, wall},
   }  
+
 
   collision = Map:new(collision)
 
@@ -105,22 +102,6 @@ end
   end
 end
 
-if love.keyboard.isDown('d') then   -- if the 'up' key is being pressed...
-  x2 = x2 + 4
-end
-if love.keyboard.isDown('w') then   -- if the 'up' key is being pressed...
-  y2 = y2 - 4
-end
-  if love.keyboard.isDown('a') then   -- if the 'up' key is being pressed...
-  x2 = x2 - 4
-end
-
-if love.keyboard.isDown('s') then 
-  y2 = y2 + 4
-end
-
-
-
 cam:setPosition(x,y)
 end
 
@@ -130,10 +111,9 @@ end
 
 function love.draw()
   cam:draw(function(l, t, w, h)
-  --map:draw()
+  map:draw()
  collision:draw()
   love.graphics.draw(playerImg, x, y)
-  love.graphics.draw(playerImg2, x2, y2)
   --love.graphics.rectangle('line', 0, 0, 64, 64)
   --love.graphics.print(hp, 0, 0)
   --end)
