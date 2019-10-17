22ba5b88d875460d2b3682eb09fd078c0d73ae5d
Abou <Asdiop17@harrisonburg.k12.va.us>
2019-10-17 09:50:44 -0400

    added enemy

--- main.lua
+++ main.lua
@@ -6,13 +6,19 @@ function love.load()
   
   x = 400
   y = 300
-  -- Enemy Coordinates
-ex = 100
-ey = 100
   playerImg = love.graphics.newImage('assets-1/player/base/gargoyle_male.png')
+
+  ex = 100
+  ey = 200
+  eimg = love.graphics.newImage('assets-1/player/base/octopode_2.png')
+
+  
   w = 67   -- The player's width is 67
   h = 67   -- The player's height is 67
   hp = 100 -- Set the player's HP to 100 at the start of the game
+
+alive = true
+
 end
 
 Cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_4.png')
@@ -44,6 +50,8 @@ function love.update(dt)
   if love.keyboard.isDown('right')  then   -- if the 'right' key is being pressed...
     x = x + 10
   end
+
+  ex = ex + 1
   
    if cc(x, y, w, h,   0, 0, 67, 67) then  
     -- if true, decrease HP:
@@ -63,6 +71,8 @@ function love.draw()
   love.graphics.rectangle('line', 50, 50, 67, 67)
   
   love.graphics.print(hp, 0, 0) 
+
+  love.graphics.draw(eimg, ex, ey)
   
 
   
