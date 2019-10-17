77791d6ad18e0a54530d8aef601281d5b9e7d9b1
sam <sostep99@harrisonburg.k12.va.us>
2019-10-17 09:54:26 -0400

    asdf

--- main.lua
+++ main.lua
@@ -1,13 +1,17 @@
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
 local Map = require 'core/map'
 local gamera = require 'core/gamera'
 local anim8 = require 'core/anim8'
 function love.load()
 
-  grid = anim8.newGrid(16, 16, spritesheet:getWidth(), spritesheet:getHeight())
-  walk = anim8.newAnimation(grid('1-6', 2), 0.2)
-
   spritesheet = love.graphics.newImage('hero/Old hero.png')
+  grid = anim8.newGrid(16, 16, spritesheet:getWidth(), spritesheet:getHeight())
+  walk = anim8.newAnimation(grid('1-6', 2), 0.01)
 
+  spritesheet2 = love.graphics.newImage('hero/2.png')
+  grid2 = anim8.newGrid(16, 16, spritesheet2:getWidth(), spritesheet2:getHeight())
+  walk2 = anim8.newAnimation(grid2('1-6', 2), 0.01)
 
   enemy_draw = false
   butterfly_alive = true
@@ -90,9 +94,15 @@ end
 
 
 function love.update(dt)
+  
+  if love.keyboard.isDown('up') or love.keyboard.isDown('right') then
   walk:update(dt)
-  grid = anim8.newGrid(16, 16, spritesheet:getWidth(), spritesheet:getHeight())
-walk = anim8.newAnimation(grid('1-6', 2), 0.2)
+ 
+  end
+  if love.keyboard.isDown('down') or love.keyboard.isDown('left') then
+    walk2:update(dt)
+   
+    end
 
   x_y = love.math.random(1,4)
 
@@ -231,9 +241,9 @@ function love.draw()
   end
   love.graphics.print(hp, 0, 0)
 
-  walk:draw()
+  walk:draw(spritesheet, x, y, 0, 2.5)
  
-  love.graphics.draw(playerImg, x, y) 
+  walk2:draw(spritesheet2, x, y, 0, 2.5)
 
   if butterfly_alive == true then
   love.graphics.draw(butterfly, butterfly_x, butterfly_y)
