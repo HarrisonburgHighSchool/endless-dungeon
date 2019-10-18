aec4821299992a79e972a672ebe36e85210000d9
Robi <rahail79@iMac714-462.local>
2019-10-18 07:49:19 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,8 +6,13 @@ function love.load()
   x = 400
   y = 300
 
-playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
-wall =love.graphics.newImage('assets-1/dungeon/wall/catacombs_0.png')
+  ex = 100
+  ey = 100
+  eimg = love.graphics.newImage('assets-1/player/base/octopode_2.png')
+
+
+  playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+  wall =love.graphics.newImage('assets-1/dungeon/wall/catacombs_0.png')
   floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_7.png')
   altar     = love.graphics.newImage('assets-1/dungeon/floor/cage_3.png')
 template = {
@@ -29,7 +34,7 @@ template = {
 
 }
 wall={
-             {wall,wall,wall,wall},
+             {wall,wall,wall,wall,wall,wall,wall,wall,wall,wall},
              {wall,'nil','nil',wall},
              {wall,'nil','nil',wall},
              {wall,'nil','nil',wall},
@@ -37,7 +42,7 @@ wall={
              {wall,'nil','nil',wall},
              {wall,'nil','nil',wall},
              {wall,'nil','nil',wall},
-             {wall,wall,wall,wall},
+             {wall,                },
 
 
             }
@@ -45,6 +50,7 @@ wall={
 w = 64   -- The player's width is 64
 h = 64   -- The player's height is 64
 hp = 100
+
   collision = Map:new(wall)
   map = Map:new(template)
   cam = gamera.new(-100, -100, 2000, 2000)
@@ -52,6 +58,7 @@ end
 
 
 function love.update(dt)
+
 if collision:cc(x,y-5,w,h, 0,0,64,64) == false then
   if love.keyboard.isDown('up')then
       y=y-5
@@ -59,17 +66,21 @@ if collision:cc(x,y-5,w,h, 0,0,64,64) == false then
   end
  end
 if collision:cc(x,y+5,w,h, 0,0,64,64) == false then
+
    if love.keyboard.isDown('down')then
     y=y+5
     cam:setPosition(x, y)
   end
 end
+
 if collision:cc(x-5,y,w,h, 0,0,64,64) == false then
+
   if love.keyboard.isDown('left')then
     x=x-5
     cam:setPosition(x, y)
   end
 end
+
  if collision:cc(x+5,y,w,h, 0,0,64,64) == false then
  if love.keyboard.isDown('right')then
     x=x+5
@@ -77,15 +88,27 @@ end
   end
  end
 
- if x < 150 and x > 50 and y > 250 and y < 250 then
-   y = y + 200
+ if x < 150 and x > 50 and y > 450 and y < 550 then
+   y = y - 200
    x = x + 400
 end
 
-if x < 150 and x > 50 and y > 350 and y < 450 then
+if x < 150 and x > 50 and y > 250 and y < 350 then
   y = y + 200
-  x = x + 400
+  x = x + 300
+
+
 end
+
+  if x < 150 and x > 50 and y > 350 and y < 450 then
+    y = y + 200
+    x = x + 300
+end
+if ex < 500 then
+   ex = ex + 1
+ end
+
+
 
 end
 
@@ -93,14 +116,22 @@ function love.draw()
 
 cam:draw(function(l, t, w, h)
   map:draw()
+
   collision:draw()
   love.graphics.print('Hello, world!', 0, 0)
 
   love.graphics.draw(playerImg, x, y)
+  love.graphics.draw(eimg, ex, ey)
 
- love.graphics.rectangle('line', 100, 300, 64, 64)
-
+  love.graphics.setColor(1, 0,0)
+  love.graphics.rectangle('line', 100, 300, 64, 64)
   love.graphics.rectangle('line', 100, 400, 64, 64)
+  love.graphics.rectangle('line', 100, 500, 64, 64)
+  love.graphics.setColor(1, 1, 1, 1)
+
+
+
+
  end)
 
 
