9d58ac4c8048b6dabab9072e7a44005db0f9cc51
Corey <ctbesh99@harrisonburg.k12.va.us>
2019-10-21 09:50:40 -0400

    dfgsdfb

--- main.lua
+++ main.lua
@@ -9,9 +9,9 @@ function love.load()
   collide1 = false
   i = 1
   g = 0
- bossdefeated = 1
- x = 200
- y = 200
+ bossdefeated = false
+ x = 64
+ y = 254
   x2 = 193
   y2 = 192
   w2 = 192
@@ -19,8 +19,8 @@ function love.load()
  w = 64
  h = 64
  HP = 100
- ex = 100
- ey = 100
+ ex = 440
+ ey = 260
  ew = 32
  eh = 32
  collide2 = false
@@ -29,10 +29,13 @@ function love.load()
  bw = 64
  bh = 64
  attack = false
- bosshealth = 6
-  
+ bosshealth = 7
+ summon = 0
+ count = 0
+  v = true
   e = love.graphics.newImage('assets-1/monster/demons/blue_devil.png')
   boss = love.graphics.newImage('assets-1/monster/demons/eye1.png')
+  boss2 = love.graphics.newImage('assets-1/monster/demons/eye2.png')
   transp = love.graphics.newImage('assets-1/dungeon/floor/tans.png')
   playerImg = love.graphics.newImage('assets-1/monster/aquatic/Sonic.png')
   playerImg2 = love.graphics.newImage('assets-1/monster/aquatic/sonic2.png')
@@ -98,6 +101,57 @@ template3 = {
             }
 
             map3 = Map:new(template3)
+template4 = {
+              {transp, transp, transp, transp, 'nil', transp, transp, transp},
+              {transp, 'nil', 'nil','nil', 'nil', 'nil', 'nil','nil', transp},
+              {transp, 'nil', 'nil','nil', 'nil', 'nil', 'nil','nil', transp},
+              {transp, 'nil', 'nil','nil', 'nil', 'nil', 'nil','nil', transp},
+              {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+              {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+              {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+              {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+              {transp, 'nil', 'nil', 'nil', colbalt, colbalt, 'nil','nil', transp},
+              {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+              {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+              {transp, transp, transp, transp, transp, transp, transp, transp},
+  
+              }
+ template5 = {
+                {transp, transp, transp, transp, 'nil', transp, transp, transp},
+                {transp, 'nil', 'nil','nil', 'nil', 'nil', 'nil','nil', transp},
+                {transp, 'nil', 'nil','nil', 'nil', 'nil', 'nil','nil', transp},
+                {transp, 'nil', 'nil','nil', 'nil', 'nil', 'nil','nil', transp},
+                {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                {transp, 'nil', 'nil', colbalt, colbalt, colbalt, 'nil','nil', transp},
+                {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                {transp, transp, transp, transp, transp, transp, transp, transp},
+    
+                }
+template6 = {
+                  {transp, transp, transp, transp, 'nil', transp, transp, transp},
+                  {transp, 'nil', 'nil','nil', 'nil', 'nil', 'nil','nil', transp},
+                  {transp, 'nil', 'nil','nil', 'nil', 'nil', 'nil','nil', transp},
+                  {transp, 'nil', 'nil','nil', 'nil', 'nil', 'nil','nil', transp},
+                  {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                  {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                  {transp, 'nil', 'nil', 'nil','nil', colbalt, 'nil','nil', transp},
+                  {transp, 'nil', 'nil', 'nil','nil', 'nil','nil','nil', transp},
+                  {transp, 'nil', 'nil', colbalt,'nil', colbalt, 'nil','nil', transp},
+                  {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                  {transp, 'nil', 'nil', 'nil','nil', 'nil', 'nil','nil', transp},
+                  {transp, transp, transp, transp, transp, transp, transp, transp},
+      
+                  }
+      
+                  map4 = Map:new(template4)
+    
+                map5 = Map:new(template5)
+  
+              map6 = Map:new(template6)
     
 end
 
@@ -112,11 +166,19 @@ function love.update(dt)
   collide1 = false
 
 
-  if map3:cc(x + 3, y, w, h) == false then
+  if count > 24 then
+    if map3:cc(x + 3, y, w, h) == false then
+      if love.keyboard.isDown('right') then  
+        x = x + 3
+     
+   
+      end
+    end
+  else
     if love.keyboard.isDown('right') then  
       x = x + 3
    
- 
+  
     end
   end
 
@@ -146,27 +208,36 @@ function love.update(dt)
     
     end
   end
+
+if(summon == 1) then
   if cc(x, y, w, h, ex, ey, ew, eh) == false then
     if x < ex then
       ex = ex - 1
     end
   end
+end
+if(summon == 1) then
   if cc(x, y, w, h, ex, ey, ew, eh) == false then
     if y > ey then
       ey = ey + 1
     end
   end
+end
+if(summon == 1) then
   if cc(x, y, w, h, ex, ey, ew, eh) == false then
     if y < ey then
       ey = ey - 1
     end
   end
+end
+if(summon == 1) then
   if cc(x, y, w, h, ex, ey, ew, eh) == true then
     HP = HP - 10
     ex = 10000000
     
     
   end
+end
 
   if(collide1 == true) then
     HP = HP - 0.1
@@ -193,7 +264,20 @@ function love.update(dt)
     x = 70
     y = 257
   end
+if(bosshealth == 0) then
+love.exitModule();
+end
 
+if bosshealth == 6 and v == true then
+  count = count + 0.1
+end
+if(count > 6) then
+  summon = 1
+  
+end 
+if(count > 20) then
+v = false
+end
   cam:setPosition(x,y)
 end
 
@@ -201,7 +285,7 @@ end
 
 
 
-
+--press a to attack
 
 
 function love.draw()
@@ -212,7 +296,7 @@ function love.draw()
   else
     map2:draw()
   end
-  map3:draw()
+ 
 
   
   if(collide1 == true) then
@@ -227,10 +311,26 @@ function love.draw()
   love.graphics.print(y, 400 ,30)
   love.graphics.print(HP, 100, 100)
   love.graphics.print(bosshealth, 400, 50)
-  love.graphics.print(tostring(collide1), 0, 0)
+  love.graphics.print(tostring(v), 0, 0)
+  if(summon == 1) then
   love.graphics.draw(e, ex, ey)
+  end
+  if(count > 6) then
+    map3:draw()
+  end
+  if(count > 10) then
+    map4:draw()
+  end
+  if(count > 14) then
+      map5:draw()
+  end
+  if(count > 3 and count < 6) then
+    love.graphics.draw(boss2, bx, by)
+  else
   love.graphics.draw(boss, bx, by)
+  end
   love.graphics.draw(playerImg, x, y)
+  love.graphics.print(count, 400, 60)
   
 end)
 end
