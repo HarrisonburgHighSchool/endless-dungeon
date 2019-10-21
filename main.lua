f199887fae48724e2a6b3f0912d56e42d8e340a8
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-10-21 09:48:40 -0400

    Fully transparent and added direction

--- main.lua
+++ main.lua
@@ -5,6 +5,9 @@ local Util = require 'core/util'
 local anim8 = require 'core/anim8'
 
 function love.load()
+  facingR = true
+  facingL = false
+  varL = true
   x = 64
   camx = 96
   mirrorx = 768
@@ -93,38 +96,42 @@ function love.load()
 end
   
 function love.update(dt)
-  if walls:cc(x + 3, y, 64, 64) == false then
-    if mirror:cc(x - 54, y, 64, 64) == false then
-      if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
-        x = x + 3
-        mirrorx = mirrorx - 3
-        walk:update(dt)
+  if love.keyboard.isDown('right') then --if the 'right' key is being pressed...
+    walk:update(dt)
+    facingR = true
+    facingL = false
+    if walls:cc(x + 3, y, 64, 64) == false then
+      if mirror:cc(x - 54, y, 64, 64) == false then 
+          x = x + 3
+          mirrorx = mirrorx - 3
+        end
       end
     end
-  end
-  if walls:cc(x, y + 3, 64, 64) == false then
-    if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
-      y = y + 3
-      mirrory = mirrory + 3
-      walk:update(dt)
-    end
-  end
-  if walls:cc(x - 3, y, 64, 64) == false then
-    if mirror:cc(x - 64, y, 64, 64) == false then
-      if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
-        x = x - 3
-        mirrorx = mirrorx + 3
-        walk:update(dt)
+  if love.keyboard.isDown('down') then   -- if the 'down' key is being pressed...
+      walk:update(dt)  
+      if walls:cc(x, y + 3, 64, 64) == false then
+          y = y + 3
+          mirrory = mirrory + 3
+        end
+      end
+  if love.keyboard.isDown('left') then   -- if the 'left' key is being pressed...
+    walk:update(dt)
+    facingR = false
+    facingL = true  
+    if walls:cc(x - 3, y, 64, 64) == false then
+      if mirror:cc(x - 64, y, 64, 64) == false then
+          x = x - 3
+          mirrorx = mirrorx + 3
+        end
       end
     end
-  end
-  if walls:cc(x, y - 3, 64, 64) == false then
     if love.keyboard.isDown('up') then   -- if the 'up' key is being pressed...
-      y = y - 3
-      mirrory = mirrory - 3
-      walk:update(dt)
+    walk:update(dt)
+    if walls:cc(x, y - 3, 64, 64) == false then
+        y = y - 3
+        mirrory = mirrory - 3
+      end
     end
-  end
   if cc(x, y, w, h, 158, 216, 80, 16) then  
     hp = hp - 1
   end
@@ -149,6 +156,11 @@ function love.draw()
   
   love.graphics.draw(mirrorPlayerImg, mirrorx, mirrory)
   love.graphics.print(hp, x, y)
-  walk:draw(spritesheet, x, y)
+  if facingR == true then
+    walk:draw(spritesheet, x, y)
+  end
+  if facingL == true then
+    walk:draw(spritesheet, x, y, rotation, -1, 1, 64, 0)
+  end
   end)
 end
