a01b6f93ab8e1568dd13f3a4786e5a36a97155a1
Jon <jstapleton463@harrisonburg.k12.va.us>
2019-10-21 11:08:41 -0400

    added module specification

--- main.lua
+++ main.lua
@@ -1,4 +1,10 @@
 require 'core/require'
+-- /////////////////////////////////////////////////////////////////////////
+-- If you want to load a specific module, change LOAD_MODULE from nil to the name of the module
+
+LOAD_MODULE = nil -- change this to the name of your file if you want to test your file
+
+-- /////////////////////////////////////////////////////////////////////////
 
 modules = love.filesystem.getDirectoryItems('modules')
 
@@ -7,8 +13,12 @@ for i = 1, #modules do
 end
 
 local n = love.math.random(1, #modules)
-require('modules/'..modules[n])
-table.remove(modules, n)
+if LOAD_MODULE then
+  require('modules/'..LOAD_MODULE)
+else
+  require('modules/'..modules[n])
+  table.remove(modules, n)
+end
 
 function love.exitModule()
   love.load = nil
@@ -16,10 +26,17 @@ function love.exitModule()
   love.draw = nil
   love.keypressed = nil
   if #modules >=1 then
-    local index = love.math.random(1, #modules)
-    local m = 'modules/'..modules[index]
+    local m
+    if LOAD_MODULE then
+      m = 'modules/'..LOAD_MODULE
+    else
+      local index = love.math.random(1, #modules)
+      m = 'modules/'..modules[index]
+      table.remove(modules, index)
+    end
     require(m)
-    table.remove(modules, index)
+  end
+  if love.load then
     love.load()
   else
     love.event.quit()
