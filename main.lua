9c3be6192bbde0612baa3b7877daa2d53d4371c6
AJ <ajmorr19@harrisonburg.k12.va.us>
2019-10-21 09:49:33 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,17 +18,21 @@ function love.load()
   cage3 = love.graphics.newImage('assets-1/dungeon/floor/floor_vines_6.png')
   wall = love.graphics.newImage ('assets-1/dungeon/wall/catacombs_0.png')
   door = love.graphics.newImage ('assets-1/dungeon/doors/runed_door.png')
-  opendoor = love.graphics.newImage ('assets-1/dungeon/doors/vgate_open_up.png')
-  door2 = love.graphics.newImage('assets-1/dungeon/doors/sealed_door.png')
+  openDoor = love.graphics.newImage ('assets-1/dungeon/doors/vgate_open_up.png')
+  closedDoor = love.graphics.newImage ('assets-1/dungeon/doors/sealed_door.png')
+
   statue1 = love.graphics.newImage ('assets-1/dungeon/statues/statue_sigmund.png')
   camo_hog = love.graphics.newImage('assets-1/monster/animals/camo_hog-1.png.png')
   jackal = love.graphics.newImage('assets-1/monster/animals/jackal.png')
   bat = love.graphics.newImage('assets-1/monster/animals/bat.png')
   raiju = love.graphics.newImage('assets-1/monster/animals/raiju.png')
   trap = love.graphics.newImage ('assets-1/dungeon/traps/shaft.png')
+  pressure = love.graphics.newImage ('assets-1/dungeon/traps/pressure_plate.png')
+
+  currentDoor = closedDoor
 
   background = {
-    {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+    {wall, wall, wall, wall, wall, nil, wall, wall, wall, wall},
     {wall, cage1, cage2, cage3, cage, cage1, cage2, cage3, cage, wall},
     {wall, cage1, cage2, cage3, cage, cage1, cage2, cage3, cage, wall},
     {wall, cage1, cage2, cage3, cage, cage1, cage2, cage3, cage, wall},
@@ -45,7 +49,7 @@ function love.load()
   }
 
 collision = {
-  {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+  {wall, wall, wall, wall, wall, nil, wall, wall, wall, wall},
     {wall, cage1, cage2, cage3, cage, cage1, cage2, cage3, cage, wall},
     {wall, cage1, cage2, cage3, cage, cage1, cage2, cage3, cage, wall},
     {wall, cage1, cage2, cage3, cage, cage1, cage2, cage3, cage, wall},
@@ -70,22 +74,35 @@ function love.update(dt)
     if collision:cc(x, y-5, 16, 16) then
       y = y - 5
     end
+    if cc(x, y, 64, 64,   200, 200, 64, 64) == true then
+      currentDoor = openDoor
+    end
   end
   if love.keyboard.isDown('s') then
     if collision:cc(x, y+5, 16, 16) then
       y = y + 5
+    end
+    if cc(x, y, 64, 64,   200, 200, 64, 64) == true then
+      currentDoor = openDoor
     end
   end
   if love.keyboard.isDown('d') then
     if collision:cc(x+5, y, 16, 16) then
       x = x + 5
     end
+    if cc(x, y, 64, 64,   200, 200, 64, 64) == true then
+      currentDoor = openDoor
+    end
   end
   if love.keyboard.isDown('a') then
     if collision:cc(x-5, y, 16, 16) then
       x = x - 5
     end
+    if cc(x, y, 64, 64,   200, 200, 64, 64) == true then
+      currentDoor = openDoor
+    end
   end
+  
 end
    
 
@@ -105,17 +122,14 @@ function love.draw()
   love.graphics.draw(trap, 320, 65, 0, 2)
 
 
-  love.graphics.draw(door2, 390, 0, 0, 2) -- Top Door
+  love.graphics.draw(door, 390, 0, 0, 2) -- Top Door
   love.graphics.draw(door, 390, 575, 0, 2) -- Bottom Door
-  love.graphics.draw(door, 0, 325, 0, 2) -- Left Door
+  love.graphics.draw(currentDoor, 0, 319, 0, 2) -- Left Door
   
   love.graphics.draw(statue1, 65, 65, 0, 2)
 
-  love.graphics.draw(camo_hog, 65, 325, 0, 2)
-  love.graphics.draw(bat, 675, 325, 0, 2)
-  love.graphics.draw(raiju, 385, 65, 0, 2)
 
-
+  love.graphics.draw(pressure, 190, 190,0,2)
 
   love.graphics.draw(playerImg, x, y, 0, 2)
   love.graphics.rectangle('line', 0, 0, 64, 64)
