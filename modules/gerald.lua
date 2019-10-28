a1dcee26335d8dd73e2d9b15281d4007461dfbd3
jerry437 <50630964+jerry437@users.noreply.github.com>
2019-10-28 14:26:29 -0400

    Update gerald.lua

--- modules/gerald.lua
+++ modules/gerald.lua
@@ -11,12 +11,12 @@ function love.load()
  ey = 100
 
  eimg = love.graphics.newImage('assets-1/player/base/octopode_2.png')
-  fire = love.graphics.newImage('assets-1/dungeon/wall/hell_1.png')
 
 playerImg = love.graphics.newImage('assets-1/player/base/kobold_male.png')
 wall =love.graphics.newImage('assets-1/dungeon/wall/catacombs_0.png')
   floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_7.png')
   altar     = love.graphics.newImage('assets-1/dungeon/floor/bog_green_2.png')
+  obstacle  = love.graphics.newImage('assets-1/dungeon/altars/ashenzari.png')
 template = {
 
              {floorTile, floorTile, floorTile, floorTile, floorTile,floorTile,floorTile,floorTile,floorTile,floorTile},
@@ -35,7 +35,7 @@ template = {
            }
 wall={
              {wall,wall,wall,wall,wall,wall,wall,wall,wall,wall},
-             {wall,'nil','nil',wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil',wall},
+             {wall,'nil','nil',wall, obstacle, obstacle, obstacle, obstacle, 'nil', 'nil',wall},
              {wall,'nil','nil',wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil',wall},
              {wall,'nil','nil',wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil',wall},
              {wall,'nil','nil',wall, 'nil', 'nil', wall, 'nil', 'nil', 'nil',wall},
@@ -46,16 +46,12 @@ wall={
              {wall,'nil','nil','nil', 'nil', 'nil', wall, 'nil', 'nil', 'nil',wall},
              {wall,'nil','nil','nil', 'nil', 'nil', wall, 'nil', 'nil', 'nil',wall},
              {wall,'nil','nil','nil', 'nil', 'nil', wall, 'nil', 'nil', 'nil','nil'},
-             {wall,'nil','nil','nil', 'nil', 'nil', wall, 'nil', 'nil', 'nil','nil'},
+             {wall,obstacle,obstacle,obstacle, obstacle, obstacle, wall, 'nil', 'nil', 'nil','nil'},
              {wall,wall,wall,wall,wall,wall,wall,wall,wall,wall},
 
             }
 
-firemap  = {
-  {fire, fire, fire, fire, fire},
-  {fire, fire, fire, fire, fire},
 
-}
 
 w = 64   -- The player's width is 64
 h = 64   -- The player's height is 64
@@ -64,8 +60,7 @@ hp = 100
   collision = Map:new(wall)
   map = Map:new(template)
   cam = gamera.new(-100, -100, 2000, 2000)
-  firemap = Map:new(firemap)
-  firemap:move(704, 64)
+
 end
 
 
@@ -88,6 +83,7 @@ if collision:cc(x-3,y,w,h, 0,0,32,32) == false then
     x=x-3
     cam:setPosition(x, y)
   end
+
 end
  if collision:cc(x+3,y,w,h, 0,0,32,32) == false then
   if love.keyboard.isDown('right')then
@@ -135,8 +131,8 @@ cam:draw(function(l, t, w, h)
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 
-  --love.graphics.draw(firesheet, fireslice, 400, 300)
-  firemap:draw()
+
+
  end)
  love.graphics.rectangle('line', 0, 0, 64, 64)
 love.graphics.print(hp, 0, 0)
