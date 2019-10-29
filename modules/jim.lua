5c32e1368d7ea616d34f80fb7617ef6470abb84e
jimmywilliamson <jwwill79@email.com>
2019-10-29 14:30:22 -0400

    added gold pieces

--- modules/jim.lua
+++ modules/jim.lua
@@ -9,15 +9,16 @@ function love.load()
   floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_12.png')
   altar     = love.graphics.newImage('assets-1/dungeon/altars/ru.png')
   doors     = love.graphics.newImage('assets-1/dungeon/doors/runed_door.png')
+  gold1     = love.graphics.newImage('assets-1/item/gold/gold_pile_1.png')
 
   template = { --a 3 x 3 map with the altar texture in the middle
-               {floorTile, floorTile, floorTile, 'nil'},
-               {floorTile, altar, floorTile, 'nil'},
-               {floorTile, floorTile, floorTile, floorTile},
-               {floorTile, floorTile, floorTile, floorTile},
-               {floorTile, floorTile, altar, 'nil'},
-               {floorTile, floorTile, floorTile, 'nil'},
-               {floorTile, doors, floorTile, 'nil'},
+               {floorTile, floorTile, floorTile, 'nil',     'nil'},
+               {floorTile, altar,     floorTile, 'nil',     'nil'},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+               {floorTile, floorTile, altar,     'nil',     'nil', floorTile, floorTile},
+               {floorTile, floorTile, floorTile, 'nil',     'nil', floorTile},
+               {floorTile, doors,     floorTile, 'nil',     'nil', floorTile, floorTile},
              }
   map = Map:new(template)
 end
@@ -25,16 +26,16 @@ end
 
 function love.update(dt)
   if love.keyboard.isDown('right') then
-    x = x + 1
+    x = x + 8
   end
   if love.keyboard.isDown('left') then
-    x = x + -2
+    x = x + -8
   end
   if love.keyboard.isDown('up') then
-    y = y + -2
+    y = y + -8
   end
   if love.keyboard.isDown('down') then
-    y = y + 2
+    y = y + 8
   end
 end
 
@@ -43,4 +44,5 @@ function love.draw()
   map:draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
+  love.graphics.draw(gold1, 400, 420)
 end
