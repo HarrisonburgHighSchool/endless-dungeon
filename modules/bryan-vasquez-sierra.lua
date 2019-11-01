756912984dd62355e5cc2a990fb17501425ee456
bryan vasquez sierra <bxvasq79@iMac763-462.local>
2019-11-01 14:29:54 -0400

    dobe

--- modules/bryan-vasquez-sierra.lua
+++ modules/bryan-vasquez-sierra.lua
@@ -13,31 +13,51 @@ function love.load()
   hp = 100
   x = 400
   y = 300
+time = 6000 -- store a big numer in time
 
   img = love.graphics.newImage('assets-1/player/base/octopode_1.png')
 ex = 100
 ey = 100
 elimg = love.graphics.newImage('assets-1/player/base/octopode_2.png')
 
-  playerImg = love.graphics.newImage('assets-2/player/transform/dragon_form.png')
+  playerImg = love.graphics.newImage('assets-1/player/transform/dragon_form.png')
   --playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
-floorTile = love.graphics.newImage('assets-1/dungeon/floor/cage_0.png')
+floorTile = love.graphics.newImage('assets-1/dungeon/floor/white_marble_1.png')
 background = {
-  {floorTile, floorTile, floorTile, floorTile},
-  {floorTile, floorTile, floorTile, floorTile},
-  {floorTile, floorTile, floorTile, floorTile},
-  {floorTile, floorTile, floorTile, floorTile},
-  {floorTile, floorTile, floorTile, floorTile},
-  {floorTile, floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
+  {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile,floorTile, floorTile, floorTile},
 }
-wall  = love.graphics.newImage('assets-1/dungeon/wall/orc_6.png')
+wall  = love.graphics.newImage('assets-1/dungeon/wall/snake_6.png')
 collision = {
-  {wall, wall, wall, wall, wall},
-  {wall, 'nil', 'nil', 'nil', 'nil'},
-  {wall, 'nil', 'nil', 'nil', 'nil'},
-  {wall, 'nil', 'nil', wall, wall},
-  {wall, 'nil', 'nil', wall, wall},
-  {wall, 'nil', 'nil', wall, wall},
+  {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, 'nil'},
+  {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil','nil', 'nil', wall, wall},
+  {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', wall, wall},
+  {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall, wall, wall},
+  {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall},
+  {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall},
+  {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall},
+  {wall, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall,'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall,'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall,'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall,'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall,'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall,'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall,'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall,'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall, wall},
+  {wall, wall, wall, wall, wall, wall,'nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall, wall},
+  {wall, wall, wall, wall, 'nil', 'nil','nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall, wall},
+  {wall, wall, wall, wall, 'nil', 'nil','nil', 'nil', 'nil', 'nil', 'nil', wall, wall, wall, wall, wall},
 }
 wall2  = love.graphics.newImage('assets-1/dungeon/wall/cobalt_stone_2.png')
 template = {--a 3 x 3 map with the altar texture in the middle
@@ -91,6 +111,10 @@ end
 end
 cam:setPosition(x, y)
 
+time = time - 1
+if time < 0 then
+  love.load()
+end
   -- Nothing to update yet
 end
 
@@ -105,6 +129,8 @@ function love.draw()
   love.graphics.print('Hello, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
   love.graphics.rectangle('line' ,50, 50, 64, 64)
-  love.exitModule();
+  if x > 600 and y > 600 then
+    love.exitModule();
+end
  end)
 end
