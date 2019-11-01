d78d32dd13cd2359f9f1e3aa9259d9edf1190bed
nali <nibala45@harrisonburg.k12.va.us>
2019-11-01 09:53:24 -0400

    Update Nali-Balak.lua

--- modules/Nali-Balak.lua
+++ modules/Nali-Balak.lua
@@ -2,31 +2,30 @@ local Map = require 'core/map'
 function love.load()
  playerImg = love.graphics.newImage('assets-1/monster/animals/fire_bat.png')
   cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_1.png')
-  cobalt = love.graphics.newImage('assets-1/dungeon/wall/cobalt_stone_8.png')
-  openDoor = love.graphics.newImage('asset-1/dungeon/doors/vgate_open_down.png')
-  closedDoor = love.graphics.newImage('asset-1/dungeon/doors/vgate_open_down.png')
-  switch = love.graphics.newImage('asset-1/dungeon/doors/vgate_open_down.png')
+  stone = love.graphics.newImage('assets-1/dungeon/wall/cobalt_stone_8.png')
+  openDoor = love.graphics.newImage('assets-1/dungeon/doors/vgate_open_down.png')
+  closedDoor = love.graphics.newImage('assets-1/dungeon/doors/vgate_open_down.png')
+  switch = love.graphics.newImage('assets-1/dungeon/doors/vgate_open_down.png')
   
   currentDoor = closedDoor
  x = 400
  y = 400
 
  floor = {
-           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
-           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
-           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
-           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
-           {cobalt, 'nil', 'nil', 'nil', 'nil', cobalt},
-           {cobalt, 'nil', 'nil', 'nil', 'nil', cobalt},
-           {cobalt, 'nil', 'nil', 'nil', 'nil', cobalt},
-           {cobalt, 'nil', 'nil', 'nil', 'nil', cobalt},
-           {cobalt, 'nil', 'nil', 'nil', 'nil', cobalt},
-           {cobalt, 'nil', 'nil', 'nil', 'nil', cobalt},
-           {cobalt, 'nil', 'nil', 'nil', 'nil', cobalt},
-           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
-           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
-           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
-           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
+           {cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt, cobalt},
  
  }
 
@@ -37,7 +36,7 @@ function love.load()
        {stone, stone, stone, stone},
        {stone, 'nil', 'nil', stone},
        {stone, 'nil', 'nil', stone},
-       {stone, 'nil', 'nil', stone},
+       {stone, 'nil', 'nil', 'nil'},
        {stone, 'nil', 'nil', stone},                                  
        {stone, stone, stone, stone},
 
@@ -48,20 +47,20 @@ end
 
 
 function love.update(dt)
-  if love.keyboard.isPressed('right')then -- if the 'up' key is being pressed
-    x = x + 15
+  if love.keyboard.isDown('right')then -- if the 'up' key is being pressed
+    x = x + 5
   end
-  if love.keyboard.isPressed('left')then -- if the 'up' key is being pressed
-    x = x - 15
+  if love.keyboard.isDown('left')then -- if the 'up' key is being pressed
+    x = x - 5
   end
-  if love.keyboard.isPressed('down')then -- if the 'up' key is being pressed
-    y = y + 15
+  if love.keyboard.isDown('down')then -- if the 'up' key is being pressed
+    y = y + 5
   end
-  if love.keyboard.isPressed('up')then -- if the 'up' key is being pressed
-  y = y - 15
+  if love.keyboard.isDown('up')then -- if the 'up' key is being pressed
+  y = y - 5
   end
   if cc(x, y, 64, 64,   200, 200, 64, 64) == true then
-  --if true then open
+  --if true then closed
  end
 
 end
@@ -75,7 +74,7 @@ function love.draw()
   love.graphics.print('Hello, world!', 10, 10)
   map:draw()
   wall:draw()
-  love.graphics.draw(playerImg, x, y)
-  love.graphics.draw(currentDoor, 500, 500)
-love.graphics.draw(switch, 200, 200)
+  love.graphics.draw(playerImg,x,y)
+  love.graphics.draw(currentDoor, 192, 190)
+love.graphics.draw(switch, 513, 190)
 end
