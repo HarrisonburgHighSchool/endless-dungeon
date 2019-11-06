07449f69a26c5c14d80b659662b8b4cfaa3c18d2
Jackson Calhoun <jwcalh19@harrisonburg.k12.va.us>
2019-11-06 09:54:22 -0500

    gggg

--- modules/jackson-calhoun.lua
+++ modules/jackson-calhoun.lua
@@ -10,32 +10,17 @@ function love.load()
   floor = love.graphics.newImage('assets-1/dungeon/floor/crypt_domino_2a.png')
   wall = love.graphics.newImage('assets-2/dc-dngn/wall/brick_dark3.png')
   door = love.graphics.newImage('assets-2/dc-dngn/dngn_open_door.png')
+  
   doorc = love.graphics.newImage('assets-2/dc-dngn/dngn_closed_door.png')
-  furnace = love.graphics.newImage('assets-2/dc-dngn/wall/stone_gray0.png')
+ 
   x = 65
   y = 70
-  w = 60   -- The player's width is 64
-  h = 60   -- The player's height is 64
+  w = 60   
+  h = 60   
   sound:setVolume(0.35)
   sound:setLooping(true)
   love.audio.play( sound )
-  --template = {
-    --{wall, wall, wall, wall, wall, wall, wall, wall, doorc, wall},
-    --{door, floor, floor, floor, wall, floor, floor, floor, floor, wall},
-    --{wall, wall, wall, floor, wall, wall, floor, wall, wall, wall},
-    --{wall, floor, wall, floor, floor, floor, floor, floor, floor, wall},
-    --{wall, floor, wall, wall, floor, wall, floor, wall, floor, wall},
-    --{wall, floor, floor, floor, floor, wall, wall, wall, wall, wall},
-    --{wall, floor, wall, floor, floor, floor, floor, floor, floor, wall},
-    --{wall, wall, wall, floor, wall, floor, wall, wall, wall, wall},
-    --{wall, floor, floor, floor, wall, floor, floor, floor, floor, wall},
-    --{wall, floor, wall, wall, wall, floor, wall, wall, floor, wall},
-    --{wall, floor, floor, wall, floor, floor, wall, floor, floor, wall},
-    --{wall, floor, floor, wall, floor, floor, wall, floor, floor, wall},
-    --{wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
-
-  --}
-
+ 
 back = {
   {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
   {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
@@ -52,13 +37,13 @@ back = {
   {floor, floor, floor, floor, floor, floor, floor, floor, floor, floor},
 }
 collision = {
-    {wall, wall, wall, wall, wall, wall, wall, wall, doorc, wall},
+    {wall, wall, wall, wall, wall, wall, wall, wall, nil, wall},
     {door, nil, nil, nil, wall, nil, nil, nil, nil, wall},
     {wall, wall, wall, nil, wall, wall, nil, wall, wall, wall},
     {wall, nil, wall, nil, nil, nil, nil, nil, nil, wall},
     {wall, nil, wall, wall, nil, wall, nil, wall, nil, wall},
     {wall, nil, nil, nil, nil, wall, wall, wall, wall, wall},
-    {wall, nil, wall, nil, nil, nil, nil, nil, furnace, wall},
+    {wall, nil, wall, nil, nil, nil, nil, nil, nil, wall},
     {wall, wall, wall, nil, wall, nil, wall, wall, wall, wall},
     {wall, nil, nil, nil, wall, nil, nil, nil, nil, wall},
     {wall, nil, wall, wall, wall, nil, wall, wall, nil, wall},
@@ -77,41 +62,50 @@ end
 
 
 function love.update(dt)
-    if love.keyboard.isDown('up') then   -- if the 'right' key is being pressed...
+    if love.keyboard.isDown('up') then   
       if map2:cc(x, y - 4, w, h) == false then
       
       y = y - 4
     end
   end  
-  if love.keyboard.isDown('down') then   -- if the 'right' key is being pressed...
+  if love.keyboard.isDown('down') then  
     if map2:cc(x, y + 4, w, h) == false then
     
     y = y + 4
   end
 end  
-if love.keyboard.isDown('right') then   -- if the 'right' key is being pressed...
+if love.keyboard.isDown('right') then   
   if map2:cc(x + 4, y, w, h) == false then
   
   x = x + 4
   end
 end  
-if love.keyboard.isDown('left') then   -- if the 'right' key is being pressed...
+if love.keyboard.isDown('left') then  
   if map2:cc(x - 4, y, w, h) == false then
   
   x = x - 4
  end
+
+
+ if cc(0, 512, 64, 64,      0, 498, 64, 64) then
+  -- something happens
+ end
+
+
+
 end  
 
-if (x > 300) then
-  love.exitModule();
-end
+--if (x > 300) then
+  --love.exitModule();
+--end
 function love.draw()
   map:draw()
   map2:draw()
   love.graphics.draw(fountain, 675, 100)
   love.graphics.draw(fountain2, 675, 500)
   love.graphics.draw(torch, 208, 75)
-  love.graphics.draw(key, 330, 275)
+  love.graphics.draw(key, 650, 275)
+  love.graphics.draw(doorc, 0, 512)
   love.graphics.setColor(0, 0, 0, 1)
   love.graphics.rectangle('fill', x + 120, y - 150, 1000000000, 1000000000)
   love.graphics.rectangle('fill', x - 90 , y + 120, 1000000000, 1000000000)
